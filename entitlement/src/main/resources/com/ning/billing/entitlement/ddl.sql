26c3c42a503d94ad20aedeb961afc2989cd039c1
Pierre-Alexandre Meyer <pierre@ning.com>
2011-11-14 14:37:41 -0800

    account: entitlement: remove hardcoded database name in ddl.sql
    This breaks internal tests.
    
    Misc.: remove trailing spaces.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- entitlement/src/main/resources/com/ning/billing/entitlement/ddl.sql
+++ entitlement/src/main/resources/com/ning/billing/entitlement/ddl.sql
@@ -1,21 +1,17 @@
-CREATE DATABASE IF NOT EXISTS killbill;
-
-USE killbill;
-
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
     id int(11) unsigned NOT NULL AUTO_INCREMENT,
     event_id char(36) NOT NULL,
     event_type varchar(9) NOT NULL,
-    user_type varchar(10) DEFAULT NULL, 
+    user_type varchar(10) DEFAULT NULL,
     created_dt datetime NOT NULL,
-    updated_dt datetime NOT NULL,    
+    updated_dt datetime NOT NULL,
     requested_dt datetime NOT NULL,
-    effective_dt datetime NOT NULL,   
+    effective_dt datetime NOT NULL,
     subscription_id char(36) NOT NULL,
-    plan_name varchar(64) DEFAULT NULL,    
+    plan_name varchar(64) DEFAULT NULL,
     phase_name varchar(128) DEFAULT NULL,
-    plist_name varchar(64) DEFAULT NULL,    
+    plist_name varchar(64) DEFAULT NULL,
     current_version int(11) DEFAULT 1,
     is_active bool DEFAULT 1,
     processing_owner char(36) DEFAULT NULL,
@@ -26,14 +22,14 @@ CREATE TABLE events (
 
 DROP TABLE IF EXISTS claimed_events;
 CREATE TABLE claimed_events (
-    sequence_id int(11) unsigned NOT NULL,    
+    sequence_id int(11) unsigned NOT NULL,
     owner_id char(36) NOT NULL,
     hostname varchar(64) NOT NULL,
     claimed_dt datetime NOT NULL,
     event_id char(36) NOT NULL,
     PRIMARY KEY(sequence_id)
 ) ENGINE=innodb;
-  
+
 
 DROP TABLE IF EXISTS subscriptions;
 CREATE TABLE subscriptions (
@@ -41,19 +37,18 @@ CREATE TABLE subscriptions (
     bundle_id char(36) NOT NULL,
     category varchar(32) NOT NULL,
     start_dt datetime NOT NULL,
-    bundle_start_dt datetime NOT NULL,    
-    active_version int(11) DEFAULT 1,   
+    bundle_start_dt datetime NOT NULL,
+    active_version int(11) DEFAULT 1,
     ctd_dt datetime DEFAULT NULL,
     ptd_dt datetime DEFAULT NULL,
-    PRIMARY KEY(id)        
+    PRIMARY KEY(id)
 ) ENGINE=innodb;
- 
+
 DROP TABLE IF EXISTS bundles;
 CREATE TABLE bundles (
     id char(36) NOT NULL,
     start_dt datetime, /*NOT NULL*/
     name varchar(64) NOT NULL,
     account_id char(36) NOT NULL,
-    PRIMARY KEY(id)        
+    PRIMARY KEY(id)
 ) ENGINE=innodb;
-
