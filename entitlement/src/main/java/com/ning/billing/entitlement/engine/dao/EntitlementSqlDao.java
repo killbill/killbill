1d4d8242cb43e1e5e86dc74e3dbd1a46d74542df
stephane brossier <stephane@ning.com>
2011-12-13 16:18:37 -0800

    Fix weird issue from jdbi/mysql where we need to explicitely add transaction. needs some follow up...

--- entitlement/src/main/java/com/ning/billing/entitlement/engine/dao/EntitlementSqlDao.java
+++ entitlement/src/main/java/com/ning/billing/entitlement/engine/dao/EntitlementSqlDao.java
@@ -71,9 +71,16 @@ public class EntitlementSqlDao implements EntitlementDao {
     }
 
     @Override
-    public SubscriptionBundle createSubscriptionBundle(SubscriptionBundleData bundle) {
-        bundlesDao.insertBundle(bundle);
-        return bundle;
+    public SubscriptionBundle createSubscriptionBundle(final SubscriptionBundleData bundle) {
+        return bundlesDao.inTransaction(new Transaction<SubscriptionBundle, BundleSqlDao>() {
+
+            @Override
+            public SubscriptionBundle inTransaction(BundleSqlDao trans,
+                    TransactionStatus status) throws Exception {
+                trans.insertBundle(bundle);
+                return trans.getBundleFromId(bundle.getId().toString());
+            }
+        });
     }
 
     @Override
