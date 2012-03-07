8d4ea3fc7485c8a35573b4f8f4d26f124dbce8d1
stephane brossier <stephane@ning.com>
2012-03-07 15:11:26 -0800

    Fix issue in the use of custom fields in Subscription

--- entitlement/src/main/java/com/ning/billing/entitlement/engine/dao/EntitlementSqlDao.java
+++ entitlement/src/main/java/com/ning/billing/entitlement/engine/dao/EntitlementSqlDao.java
@@ -511,7 +511,7 @@ public class EntitlementSqlDao implements EntitlementDao {
             default:
                 break;
             }
-            loadCustomFields(reloaded);
+            loadCustomFields((SubscriptionData) reloaded);
             result.add(reloaded);
         }
         return result;
@@ -618,13 +618,12 @@ public class EntitlementSqlDao implements EntitlementDao {
         });
     }
 
-
-    private void loadCustomFields(final Subscription subscription) {
+    private void loadCustomFields(final SubscriptionData subscription) {
         FieldStoreDao fieldStoreDao = subscriptionsDao.become(FieldStoreDao.class);
         List<CustomField> fields = fieldStoreDao.load(subscription.getId().toString(), subscription.getObjectName());
-        subscription.clearFields();
+        subscription.clearFieldsInternal(false);
         if (fields != null) {
-            subscription.addFields(fields);
+            subscription.addFieldsInternal(fields, false);
         }
     }
 }
