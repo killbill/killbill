3a5b948ba851dc64008116bbdac846510602c5a1
Pierre-Alexandre Meyer <pierre@ning.com>
2012-06-07 18:12:07 -0700

    jaxrs: add test for AccountJsonSimple
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- jaxrs/src/main/java/com/ning/billing/jaxrs/json/AccountJsonSimple.java
+++ jaxrs/src/main/java/com/ning/billing/jaxrs/json/AccountJsonSimple.java
@@ -15,18 +15,17 @@
  */
 package com.ning.billing.jaxrs.json;
 
-import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
 
 public class AccountJsonSimple {
-
     @JsonView(BundleTimelineViews.Base.class)
     protected final String accountId;
-    
+
     @JsonView(BundleTimelineViews.Base.class)
     protected final String externalKey;
-    
+
     public AccountJsonSimple() {
         this.accountId = null;
         this.externalKey = null;
@@ -34,7 +33,7 @@ public class AccountJsonSimple {
 
     @JsonCreator
     public AccountJsonSimple(@JsonProperty("accountId") String accountId,
-            @JsonProperty("externalKey") String externalKey) {
+                             @JsonProperty("externalKey") String externalKey) {
         this.accountId = accountId;
         this.externalKey = externalKey;
     }
@@ -45,5 +44,33 @@ public class AccountJsonSimple {
 
     public String getExternalKey() {
         return externalKey;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final AccountJsonSimple that = (AccountJsonSimple) o;
+
+        if (accountId != null ? !accountId.equals(that.accountId) : that.accountId != null) {
+            return false;
+        }
+        if (externalKey != null ? !externalKey.equals(that.externalKey) : that.externalKey != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = accountId != null ? accountId.hashCode() : 0;
+        result = 31 * result + (externalKey != null ? externalKey.hashCode() : 0);
+        return result;
     }
 }
