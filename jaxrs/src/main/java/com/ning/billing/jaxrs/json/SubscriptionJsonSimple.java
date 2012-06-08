b9a50b689e49611219bced2f3132fa60e384e980
Pierre-Alexandre Meyer <pierre@ning.com>
2012-06-08 13:10:39 -0700

    jaxrs: add test for SubscriptionJsonSimple
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- jaxrs/src/main/java/com/ning/billing/jaxrs/json/SubscriptionJsonSimple.java
+++ jaxrs/src/main/java/com/ning/billing/jaxrs/json/SubscriptionJsonSimple.java
@@ -37,4 +37,27 @@ public class SubscriptionJsonSimple {
     public String getSubscriptionId() {
         return subscriptionId;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final SubscriptionJsonSimple that = (SubscriptionJsonSimple) o;
+
+        if (subscriptionId != null ? !subscriptionId.equals(that.subscriptionId) : that.subscriptionId != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return subscriptionId != null ? subscriptionId.hashCode() : 0;
+    }
 }
