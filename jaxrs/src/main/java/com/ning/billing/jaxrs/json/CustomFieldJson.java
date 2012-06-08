d77b2dd4a04818fe4d01cb913725bf85fb44656c
Pierre-Alexandre Meyer <pierre@ning.com>
2012-06-08 11:39:26 -0700

    jaxrs: add test for CustomFieldJson
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- jaxrs/src/main/java/com/ning/billing/jaxrs/json/CustomFieldJson.java
+++ jaxrs/src/main/java/com/ning/billing/jaxrs/json/CustomFieldJson.java
@@ -48,4 +48,32 @@ public class CustomFieldJson {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final CustomFieldJson that = (CustomFieldJson) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) {
+            return false;
+        }
+        if (value != null ? !value.equals(that.value) : that.value != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
 }
