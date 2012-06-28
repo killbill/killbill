9866aa0213b0f0b1c2955bf0fb890f1908cd769f
Pierre-Alexandre Meyer <pierre@ning.com>
2012-06-28 13:18:58 -0700

    entitlement: update SubscriptionEvents to use List, not LinkedList
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- entitlement/src/main/java/com/ning/billing/entitlement/api/user/SubscriptionEvents.java
+++ entitlement/src/main/java/com/ning/billing/entitlement/api/user/SubscriptionEvents.java
@@ -17,21 +17,19 @@
 package com.ning.billing.entitlement.api.user;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 import com.ning.billing.entitlement.events.EntitlementEvent;
 
 public class SubscriptionEvents {
-
     public static final long INITIAL_VERSION = 1;
 
-    private final LinkedList<EntitlementEvent> events;
+    private final List<EntitlementEvent> events;
 
     private long activeVersion;
 
-    public SubscriptionEvents(final UUID subscriptionId) {
-        super();
-        final UUID subscriptionId1 = subscriptionId;
+    public SubscriptionEvents() {
         this.events = new LinkedList<EntitlementEvent>();
         this.activeVersion = INITIAL_VERSION;
     }
@@ -40,21 +38,20 @@ public class SubscriptionEvents {
         events.add(ev);
     }
 
-    public LinkedList<EntitlementEvent> getCurrentView() {
+    public List<EntitlementEvent> getCurrentView() {
         return getViewForVersion(activeVersion);
     }
 
-    public LinkedList<EntitlementEvent> getViewForVersion(final long version) {
-
+    public List<EntitlementEvent> getViewForVersion(final long version) {
         final LinkedList<EntitlementEvent> result = new LinkedList<EntitlementEvent>();
         for (final EntitlementEvent cur : events) {
             if (cur.getActiveVersion() == version) {
                 result.add(cur);
             }
         }
+
         return result;
     }
-
 
     public long getActiveVersion() {
         return activeVersion;
@@ -62,5 +59,43 @@ public class SubscriptionEvents {
 
     public void setActiveVersion(final long activeVersion) {
         this.activeVersion = activeVersion;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("SubscriptionEvents");
+        sb.append("{activeVersion=").append(activeVersion);
+        sb.append(", events=").append(events);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final SubscriptionEvents that = (SubscriptionEvents) o;
+
+        if (activeVersion != that.activeVersion) {
+            return false;
+        }
+        if (events != null ? !events.equals(that.events) : that.events != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = events != null ? events.hashCode() : 0;
+        result = 31 * result + (int) (activeVersion ^ (activeVersion >>> 32));
+        return result;
     }
 }
