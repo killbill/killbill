6c05faf3ab28cc3efc1f4e47e0a5ed12b1691ec3
Pierre-Alexandre Meyer <pierre@ning.com>
2012-06-28 10:48:16 -0700

    api: add RequestedSubscriptionEvent
    This event will be fired from the entitlement module for future changes.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- api/src/main/java/com/ning/billing/entitlement/api/user/RequestedSubscriptionEvent.java
+++ api/src/main/java/com/ning/billing/entitlement/api/user/RequestedSubscriptionEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2011 Ning, Inc.
+ * Copyright 2010-2012 Ning, Inc.
  *
  * Ning licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
@@ -16,5 +16,5 @@
 
 package com.ning.billing.entitlement.api.user;
 
-public interface EffectiveSubscriptionEvent extends SubscriptionEvent {
+public interface RequestedSubscriptionEvent extends SubscriptionEvent {
 }
---
+++ api/src/main/java/com/ning/billing/entitlement/api/user/SubscriptionEvent.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010-2012 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.billing.entitlement.api.user;
+
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+
+import com.ning.billing.entitlement.api.SubscriptionTransitionType;
+import com.ning.billing.entitlement.api.user.Subscription.SubscriptionState;
+import com.ning.billing.util.bus.BusEvent;
+
+public interface SubscriptionEvent extends BusEvent {
+    UUID getId();
+
+    SubscriptionTransitionType getTransitionType();
+
+    UUID getBundleId();
+
+    UUID getSubscriptionId();
+
+    DateTime getSubscriptionStartDate();
+
+    DateTime getRequestedTransitionTime();
+
+    DateTime getEffectiveTransitionTime();
+
+    SubscriptionState getPreviousState();
+
+    String getPreviousPlan();
+
+    String getPreviousPriceList();
+
+    String getPreviousPhase();
+
+    String getNextPlan();
+
+    String getNextPhase();
+
+    SubscriptionState getNextState();
+
+    String getNextPriceList();
+
+    Integer getRemainingEventsForUserOperation();
+
+    Long getTotalOrdering();
+}
