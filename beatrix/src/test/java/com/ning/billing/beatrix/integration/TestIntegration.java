248a596ddc95bebebc31867d360ec5f28d889403
stephane brossier <stephane@ning.com>
2012-07-06 15:16:19 -0700

    On going work for fixing disabled Beatrix tests

--- beatrix/src/test/java/com/ning/billing/beatrix/integration/TestIntegration.java
+++ beatrix/src/test/java/com/ning/billing/beatrix/integration/TestIntegration.java
@@ -93,66 +93,7 @@ public class TestIntegration extends TestIntegrationBase {
     }
 
 
-//    // STEPH set to disabled until test written properly and fixed
-//    @Test(groups = "slow", enabled = true)
-//    public void testRepairChangeBPWithAddonIncluded() throws Exception {
-//        
-//        log.info("Starting testRepairChangeBPWithAddonIncluded");
-//        
-//        DateTime initialDate = new DateTime(2012, 4, 25, 0, 3, 42, 0);
-//        clock.setDeltaFromReality(initialDate.getMillis() - clock.getUTCNow().getMillis());
-//        
-//        Account account = createAccountWithPaymentMethod(getAccountData(25));
-//        assertNotNull(account);
-//
-//        SubscriptionBundle bundle = entitlementUserApi.createBundleForAccount(account.getId(), "whatever", context);
-//
-//        String productName = "Shotgun";
-//        BillingPeriod term = BillingPeriod.MONTHLY;
-//        String planSetName = PriceListSet.DEFAULT_PRICELIST_NAME;
-//
-//        busHandler.pushExpectedEvent(NextEvent.CREATE);
-//        busHandler.pushExpectedEvent(NextEvent.INVOICE);
-//        SubscriptionData baseSubscription = subscriptionDataFromSubscription(entitlementUserApi.createSubscription(bundle.getId(),
-//                new PlanPhaseSpecifier(productName, ProductCategory.BASE, term, planSetName, null), null, context));
-//        assertNotNull(baseSubscription);
-//        assertTrue(busHandler.isCompleted(DELAY));
-//   
-//        // MOVE CLOCK A LITTLE BIT-- STILL IN TRIAL
-//        Interval it = new Interval(clock.getUTCNow(), clock.getUTCNow().plusDays(3));
-//        clock.addDeltaFromReality(it.toDurationMillis());
-//
-//        busHandler.pushExpectedEvent(NextEvent.CREATE);
-//        busHandler.pushExpectedEvent(NextEvent.INVOICE);
-//        busHandler.pushExpectedEvent(NextEvent.PAYMENT);
-//        subscriptionDataFromSubscription(entitlementUserApi.createSubscription(bundle.getId(),
-//                new PlanPhaseSpecifier("Telescopic-Scope", ProductCategory.ADD_ON, BillingPeriod.MONTHLY, PriceListSet.DEFAULT_PRICELIST_NAME, null), null, context));
-//        assertTrue(busHandler.isCompleted(DELAY));
-//        
-//        busHandler.pushExpectedEvent(NextEvent.CREATE);
-//        busHandler.pushExpectedEvent(NextEvent.INVOICE);
-//        busHandler.pushExpectedEvent(NextEvent.PAYMENT);
-//        subscriptionDataFromSubscription(entitlementUserApi.createSubscription(bundle.getId(),
-//                new PlanPhaseSpecifier("Laser-Scope", ProductCategory.ADD_ON, BillingPeriod.MONTHLY, PriceListSet.DEFAULT_PRICELIST_NAME, null), null, context)); 
-//        assertTrue(busHandler.isCompleted(DELAY));
-//        
-//
-//        // 26 / 5
-//        int duration = 28;
-//        it = new Interval(clock.getUTCNow(), clock.getUTCNow().plusDays(duration));
-//        busHandler.pushExpectedEvent(NextEvent.PHASE);
-//        busHandler.pushExpectedEvent(NextEvent.PHASE);
-//        busHandler.pushExpectedEvent(NextEvent.PHASE);            
-//        busHandler.pushExpectedEvent(NextEvent.INVOICE);
-//        busHandler.pushExpectedEvent(NextEvent.PAYMENT);
-//        clock.addDeltaFromReality(it.toDurationMillis());
-//        assertTrue(busHandler.isCompleted(DELAY));
-//        
-//        assertListenerStatus();
-//    }
-
-    // STEPH set to disabled until test written properly and fixed
-    @Test(groups = "slow", enabled = false)
+    @Test(groups = "slow", enabled = true)
     public void testRepairChangeBPWithAddonIncluded() throws Exception {
 
         log.info("Starting testRepairChangeBPWithAddonIncluded");
@@ -199,7 +140,7 @@ public class TestIntegration extends TestIntegrationBase {
         assertTrue(busHandler.isCompleted(DELAY));
         assertListenerStatus();
 
-        // MOVE CLOCK A LITTLE BIT MORE -- EITHER STAY IN TRIAL OR GET OUT   
+        // MOVE CLOCK A LITTLE BIT MORE -- EITHER STAY IN TRIAL OR GET OUT
         busHandler.pushExpectedEvent(NextEvent.PHASE);
         busHandler.pushExpectedEvent(NextEvent.PHASE);
         busHandler.pushExpectedEvent(NextEvent.INVOICE);
