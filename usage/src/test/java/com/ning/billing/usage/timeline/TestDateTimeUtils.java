fa19bcb81c03f04c0091886dc443c873f8f2d47a
Pierre-Alexandre Meyer <pierre@mouraf.org>
2012-07-28 11:15:54 -0700

    usage: initial import of DateTimeUtils
    Utility class to store time samples.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

---
+++ usage/src/test/java/com/ning/billing/usage/timeline/TestDateTimeUtils.java
@@ -0,0 +1,23 @@
+package com.ning.billing.usage.timeline;
+
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.ning.billing.util.clock.Clock;
+import com.ning.billing.util.clock.ClockMock;
+
+public class TestDateTimeUtils {
+
+    private final Clock clock = new ClockMock();
+
+    @Test(groups = "fast")
+    public void testRoundTrip() throws Exception {
+        final DateTime utcNow = clock.getUTCNow();
+        final int unixSeconds = DateTimeUtils.unixSeconds(utcNow);
+        final DateTime dateTimeFromUnixSeconds = DateTimeUtils.dateTimeFromUnixSeconds(unixSeconds);
+
+        Assert.assertEquals(Seconds.secondsBetween(dateTimeFromUnixSeconds, utcNow), 0);
+    }
+}
