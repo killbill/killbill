9f5ff8d9ecb7bf1bd22441e2bc377ab3d89e660f
Pierre-Alexandre Meyer <pierre@ning.com>
2012-07-30 11:18:01 -0700

    jaxrs: add AuditLogJson
    This json object will contain audit log entries.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

---
+++ jaxrs/src/test/java/com/ning/billing/jaxrs/json/TestAuditLogJson.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2010-2012 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.billing.jaxrs.json;
+
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.ning.billing.jaxrs.JaxrsTestSuite;
+import com.ning.billing.util.clock.Clock;
+import com.ning.billing.util.clock.DefaultClock;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+public class TestAuditLogJson extends JaxrsTestSuite {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private final Clock clock = new DefaultClock();
+
+    static {
+        mapper.registerModule(new JodaModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    @Test(groups = "fast")
+    public void testJson() throws Exception {
+        final String changeType = UUID.randomUUID().toString();
+        final DateTime changeDate = clock.getUTCNow();
+        final String changedBy = UUID.randomUUID().toString();
+        final String reasonCode = UUID.randomUUID().toString();
+        final String comments = UUID.randomUUID().toString();
+        final String userToken = UUID.randomUUID().toString();
+
+        final AuditLogJson auditLogJson = new AuditLogJson(changeType, changeDate, changedBy, reasonCode, comments, userToken);
+        Assert.assertEquals(auditLogJson.getChangeType(), changeType);
+        Assert.assertEquals(auditLogJson.getChangeDate(), changeDate);
+        Assert.assertEquals(auditLogJson.getChangedBy(), changedBy);
+        Assert.assertEquals(auditLogJson.getReasonCode(), reasonCode);
+        Assert.assertEquals(auditLogJson.getComments(), comments);
+        Assert.assertEquals(auditLogJson.getUserToken(), userToken);
+
+        final String asJson = mapper.writeValueAsString(auditLogJson);
+        Assert.assertEquals(asJson, "{\"changeType\":\"" + auditLogJson.getChangeType() + "\"," +
+                                    "\"changeDate\":\"" + auditLogJson.getChangeDate().toDateTimeISO().toString() + "\"," +
+                                    "\"changedBy\":\"" + auditLogJson.getChangedBy() + "\"," +
+                                    "\"reasonCode\":\"" + auditLogJson.getReasonCode() + "\"," +
+                                    "\"comments\":\"" + auditLogJson.getComments() + "\"," +
+                                    "\"userToken\":\"" + auditLogJson.getUserToken() + "\"}");
+
+        final AuditLogJson fromJson = mapper.readValue(asJson, AuditLogJson.class);
+        Assert.assertEquals(fromJson, auditLogJson);
+    }
+}
