9e8344a27b2e14de45d1e68e5a5eb278de846de3
stephane brossier <stephane@ning.com>
2012-10-21 13:49:31 -0700

    Intermediary commit in order to switch

---
+++ util/src/main/java/com/ning/billing/util/events/BusInternalEvent.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010-2011 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.billing.util.events;
+
+import java.util.UUID;
+
+public interface BusInternalEvent {
+
+    public enum BusEventType {
+        ACCOUNT_CREATE,
+        ACCOUNT_CHANGE,
+        SUBSCRIPTION_TRANSITION,
+        BUNDLE_REPAIR,
+        INVOICE_EMPTY,
+        INVOICE_CREATION,
+        INVOICE_ADJUSTMENT,
+        PAYMENT_INFO,
+        PAYMENT_ERROR,
+        CONTROL_TAG_CREATION,
+        CONTROL_TAG_DELETION,
+        USER_TAG_CREATION,
+        USER_TAG_DELETION,
+        CONTROL_TAGDEFINITION_CREATION,
+        CONTROL_TAGDEFINITION_DELETION,
+        USER_TAGDEFINITION_CREATION,
+        USER_TAGDEFINITION_DELETION,
+        OVERDUE_CHANGE
+    }
+
+    public BusEventType getBusEventType();
+
+    public UUID getUserToken();
+
+    public Long getTenantRecordId();
+
+    public Long getAccountRecordId();
+}
