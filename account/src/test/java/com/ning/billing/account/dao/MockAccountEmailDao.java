afb5e5224ba96def962d3f6e2bb6c729c6e4faa3
Pierre-Alexandre Meyer <pierre@ning.com>
2012-11-09 10:26:01 -0800

    test: fix TestDefaultAccountUserApi#testAddEmail failure
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- account/src/test/java/com/ning/billing/account/dao/MockAccountEmailDao.java
+++ account/src/test/java/com/ning/billing/account/dao/MockAccountEmailDao.java
@@ -16,69 +16,28 @@
 
 package com.ning.billing.account.dao;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 import com.ning.billing.account.api.AccountApiException;
 import com.ning.billing.account.api.AccountEmail;
-import com.ning.billing.util.callcontext.InternalCallContext;
 import com.ning.billing.util.callcontext.InternalTenantContext;
-import com.ning.billing.util.entity.EntityPersistenceException;
+import com.ning.billing.util.entity.dao.MockEntityDaoBase;
 
-import com.google.common.collect.ImmutableList;
-
-public class MockAccountEmailDao implements AccountEmailDao {
-
-    private final Map<UUID, Set<AccountEmail>> emails = new ConcurrentHashMap<UUID, Set<AccountEmail>>();
-
-    @Override
-    public void create(final AccountEmail entity, final InternalCallContext context) throws AccountApiException {
-        Set<AccountEmail> theSet = emails.get(entity.getAccountId());
-        theSet.add(entity);
-    }
-
-    @Override
-    public Long getRecordId(final UUID id, final InternalTenantContext context) {
-        return 1L;
-    }
-
-    @Override
-    public AccountEmail getById(final UUID id, final InternalTenantContext context) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public List<AccountEmail> get(final InternalTenantContext context) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void delete(final AccountEmail email, final InternalCallContext context) {
-        Set<AccountEmail> theSet = emails.get(email.getAccountId());
-        theSet.remove(email);
-    }
+public class MockAccountEmailDao extends MockEntityDaoBase<AccountEmail, AccountApiException> implements AccountEmailDao {
 
     @Override
     public List<AccountEmail> getByAccountId(final UUID accountId, final InternalTenantContext context) {
-        final Set<AccountEmail> accountEmails = emails.get(accountId);
-        if (accountEmails == null) {
-            return ImmutableList.<AccountEmail>of();
-        } else {
-            return ImmutableList.<AccountEmail>copyOf(accountEmails.iterator());
+        final List<AccountEmail> accountEmails = new ArrayList<AccountEmail>();
+        for (final Map<Long, AccountEmail> accountEmail : entities.values()) {
+            final AccountEmail email = accountEmail.values().iterator().next();
+            if (email.getAccountId().equals(accountId)) {
+                accountEmails.add(email);
+            }
         }
-    }
 
-    @Override
-    public void test(final InternalTenantContext context) {
-    }
-
-    @Override
-    public AccountEmail getByRecordId(final Long recordId,
-            final InternalTenantContext context) {
-        return null;
+        return accountEmails;
     }
 }
