ac62dfcaa6cf2b4ea7a2c8e16f10476e85262036
Pierre-Alexandre Meyer <pierre@ning.com>
2013-05-29 17:30:35 -0700

    analytics: pivot by phase type in cancellations_per_day report
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- osgi-bundles/bundles/analytics/src/main/resources/reports/cancellations_per_day.sql
+++ osgi-bundles/bundles/analytics/src/main/resources/reports/cancellations_per_day.sql
@@ -1,10 +1,11 @@
 create or replace view v_cancellations_per_day as
 select
-  date_format(next_start_date, '%Y-%m-%d') as day
+  prev_phase as pivot
+, date_format(next_start_date, '%Y-%m-%d') as day
 , count(*) as count
 from bst
 where event = 'CANCEL_BASE'
 and report_group = 'default'
-group by 1
-order by 1 asc
+group by 1, 2
+order by 1, 2 asc
 ;
