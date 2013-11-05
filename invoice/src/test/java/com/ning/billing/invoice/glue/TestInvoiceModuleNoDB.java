615363d297e362b767420821a026bea914050015
stephane brossier <stephane@ning.com>
2013-11-04 18:35:20 -0800

    Fix broken test on java-sdk7

--- invoice/src/test/java/com/ning/billing/invoice/glue/TestInvoiceModuleNoDB.java
+++ invoice/src/test/java/com/ning/billing/invoice/glue/TestInvoiceModuleNoDB.java
@@ -17,12 +17,12 @@
 package com.ning.billing.invoice.glue;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.tz.UTCProvider;
 import org.mockito.Mockito;
 import org.skife.config.ConfigSource;
 
@@ -59,9 +59,13 @@ public class TestInvoiceModuleNoDB extends TestInvoiceModule {
         bind(AccountInternalApi.class).toInstance(Mockito.mock(AccountInternalApi.class));
         bind(AccountUserApi.class).toInstance(Mockito.mock(AccountUserApi.class));
 
+        installCurrencyConversionApi();
+    }
+
+    private void installCurrencyConversionApi() {
         final CurrencyConversionApi currencyConversionApi = Mockito.mock(CurrencyConversionApi.class);
         final CurrencyConversion currencyConversion = Mockito.mock(CurrencyConversion.class);
-        final Set<Rate> rates = new TreeSet<Rate>();
+        final Set<Rate> rates = new HashSet<Rate>();
         rates.add(new Rate() {
             @Override
             public Currency getBaseCurrency() {
@@ -87,7 +91,7 @@ public class TestInvoiceModuleNoDB extends TestInvoiceModule {
         try {
             Mockito.when(currencyConversionApi.getCurrencyConversion(Mockito.<Currency>any(), Mockito.<DateTime>any())).thenReturn(currencyConversion);
         } catch (CurrencyConversionException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
 
         bind(CurrencyConversionApi.class).toInstance(currencyConversionApi);
