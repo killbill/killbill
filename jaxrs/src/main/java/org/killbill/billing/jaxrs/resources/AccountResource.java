adbf2d4d5f9f06d391526cfb1e2b6a5fc172c84d
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-11-01 23:51:56 -0400

    jaxrs: fix NPE in case of aborted payments
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/AccountResource.java
+++ jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/AccountResource.java
@@ -752,6 +752,10 @@ public class AccountResource extends JaxRsResourceBase {
             default:
                 return Response.status(Status.PRECONDITION_FAILED).entity("TransactionType " + transactionType + " is not allowed for an account").build();
         }
+        // Aborted payment?
+        if (result == null) {
+            return Response.noContent().build();
+        }
         return uriBuilder.buildResponse(PaymentResource.class, "getPayment", result.getId(), uriInfo.getBaseUri().toString());
     }
 
