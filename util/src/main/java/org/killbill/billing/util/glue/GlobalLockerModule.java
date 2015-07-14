54e9076fa9330c6ab0c4d114ad2199e012663855
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-07-14 07:11:08 -0700

    util: add missing killbill-embeddeddb-postgresql dependency
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- util/src/main/java/org/killbill/billing/util/glue/GlobalLockerModule.java
+++ util/src/main/java/org/killbill/billing/util/glue/GlobalLockerModule.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010-2011 Ning, Inc.
- * Copyright 2014 Groupon, Inc
- * Copyright 2014 The Billing Project, LLC
+ * Copyright 2014-2015 Groupon, Inc
+ * Copyright 2014-2015 The Billing Project, LLC
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
