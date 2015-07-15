8d7d78d771504ab9e05e14676c0353030003e219
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-07-15 05:40:06 -0700

    util: update copyright headers
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- util/src/main/java/org/killbill/billing/util/cache/ObjectIdCacheLoader.java
+++ util/src/main/java/org/killbill/billing/util/cache/ObjectIdCacheLoader.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2014 Groupon, Inc
- * Copyright 2014 The Billing Project, LLC
+ * Copyright 2014-2015 Groupon, Inc
+ * Copyright 2014-2015 The Billing Project, LLC
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
