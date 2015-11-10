d8f716544a3ccc2f88c99f824e883228ed951ae4
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-11-10 07:41:52 -0800

    util: update to latest org.mariadb.jdbc.MySQLDataSource
    setURL now takes precedence over other setters.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- util/src/test/java/org/killbill/billing/util/dao/TestDateInvestigation.java
+++ util/src/test/java/org/killbill/billing/util/dao/TestDateInvestigation.java
@@ -193,11 +193,11 @@ public class TestDateInvestigation /* extends UtilTestSuiteWithEmbeddedDB */ {
 
     private DataSource getRawMariaDBDataSource(final String dbName, final String user, final String pwd) throws SQLException {
         final org.mariadb.jdbc.MySQLDataSource rawSource = new org.mariadb.jdbc.MySQLDataSource();
+        rawSource.setURL("jdbc:mysql://localhost:3306/killbill?createDatabaseIfNotExist=true&allowMultiQueries=true");
         rawSource.setDatabaseName(dbName);
         rawSource.setUser(user);
         rawSource.setPassword(pwd);
         rawSource.setPort(3306);
-        rawSource.setURL("jdbc:mysql://localhost:3306/killbill?createDatabaseIfNotExist=true&allowMultiQueries=true");
         return rawSource;
     }
 
