da0150e27f601c3f7f23c1c059ddd254611a3ea2
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-11-11 12:37:18 -0800

    util: catalog: don't use @UseStringTemplate3StatementLocator
    Make sure to use @EntitySqlDaoStringTemplate instead, which takes
    care of caching (the annotation should probably be renamed).
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- util/src/main/java/org/killbill/billing/util/dao/NonEntitySqlDao.java
+++ util/src/main/java/org/killbill/billing/util/dao/NonEntitySqlDao.java
@@ -1,7 +1,9 @@
 /*
  * Copyright 2010-2013 Ning, Inc.
+ * Copyright 2014-2015 Groupon, Inc
+ * Copyright 2014-2015 The Billing Project, LLC
  *
- * Ning licenses this file to you under the Apache License, version 2.0
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
@@ -18,17 +20,16 @@ package org.killbill.billing.util.dao;
 
 import java.util.UUID;
 
+import org.killbill.billing.callcontext.InternalTenantContext;
+import org.killbill.billing.util.entity.dao.EntitySqlDaoStringTemplate;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.BindBean;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.customizers.Define;
 import org.skife.jdbi.v2.sqlobject.mixins.CloseMe;
 import org.skife.jdbi.v2.sqlobject.mixins.Transactional;
-import org.skife.jdbi.v2.sqlobject.stringtemplate.UseStringTemplate3StatementLocator;
 
-import org.killbill.billing.callcontext.InternalTenantContext;
-
-@UseStringTemplate3StatementLocator
+@EntitySqlDaoStringTemplate
 public interface NonEntitySqlDao extends Transactional<NonEntitySqlDao>, CloseMe {
 
     @SqlQuery
