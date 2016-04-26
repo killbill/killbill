2e670eedb48fe59cd1453f80ba53416c588aee44
Pierre-Alexandre Meyer <pierre@mouraf.org>
2016-04-26 10:33:59 -0700

    payment: don't share the CompletionTaskBase logger
    See also See https://github.com/killbill/killbill/issues/394.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- payment/src/main/java/org/killbill/billing/payment/core/janitor/CompletionTaskBase.java
+++ payment/src/main/java/org/killbill/billing/payment/core/janitor/CompletionTaskBase.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2014-2015 Groupon, Inc
- * Copyright 2014-2015 The Billing Project, LLC
+ * Copyright 2014-2016 Groupon, Inc
+ * Copyright 2014-2016 The Billing Project, LLC
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
@@ -48,7 +48,7 @@ import org.slf4j.LoggerFactory;
 
 abstract class CompletionTaskBase<T> implements Runnable {
 
-    protected Logger log = LoggerFactory.getLogger(CompletionTaskBase.class);
+    private static final Logger log = LoggerFactory.getLogger(CompletionTaskBase.class);
 
     protected final PaymentConfig paymentConfig;
     protected final Clock clock;
