killbill/killbill

commit 2c730de94c1df507d62a43f176a9dd94cd89470c

catalog: Fixes #464
Instead of adding the new api 'findPricelist(final String name, final DateTime requestedDate, final DateTime subscriptionStartDate)' we enhanced the Plan api to also return
the pricelist on which it was defined: It is not possible to implement the api described in the bug because the subscriptionStartDate is only meaningful for the plan asssociated
with the subscription, and so the api would also require to pass the Plan as an argument.

The DefaultSubscriptionBase hack is now fixed properly and in addition i fixed some issues with existing test catalog missing pricelist entries

catalog/src/main/java/org/killbill/billing/catalog/DefaultPlan.java
@@ -38,6 +38,7 @@ import org.killbill.billing.catalog.api.PhaseType;
 import org.killbill.billing.catalog.api.Plan;
 import org.killbill.billing.catalog.api.PlanPhase;
 import org.killbill.billing.catalog.api.PlanPhasePriceOverride;
+import org.killbill.billing.catalog.api.PriceList;
 import org.killbill.billing.catalog.api.Product;
 import org.killbill.billing.catalog.api.Recurring;
 import org.killbill.xmlloader.ValidatingConfig;
@@ -73,6 +74,7 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
     @XmlElement(required = false)
     private Integer plansAllowedInBundle = 1;
 
+    private PriceList priceList;
 
     public DefaultPlan() {
         initialPhases = new DefaultPlanPhase[0];
@@ -88,7 +90,7 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
             initialPhases[i] = newPhase;
         }
         this.finalPhase = new DefaultPlanPhase(this, in.getFinalPhase(), overrides[overrides.length - 1]);
-
+        this.priceList = in.getPriceList();
     }
     /* (non-Javadoc)
       * @see org.killbill.billing.catalog.IPlan#getEffectiveDateForExistingSubscriptons()
@@ -111,6 +113,11 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
     @Override
     public Product getProduct() {
         return product;
+    }
+
+    @Override
+    public PriceList getPriceList() {
+        return priceList;
     }
 
     /* (non-Javadoc)
@@ -189,7 +196,10 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
                 p.initialize(catalog, sourceURI);
             }
         }
+        this.priceList = findPriceListForPlan(catalog);
     }
+
+
 
     @Override
     public ValidationErrors validate(final StandaloneCatalog catalog, final ValidationErrors errors) {
@@ -223,6 +233,11 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
 
     public DefaultPlan setProduct(final DefaultProduct product) {
         this.product = product;
+        return this;
+    }
+
+    public DefaultPlan setPriceList(final DefaultPriceList priceList) {
+        this.priceList = priceList;
         return this;
     }
 
@@ -306,5 +321,16 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
                 + effectiveDateForExistingSubscriptons + ", product=" + product + ", initialPhases="
                 + Arrays.toString(initialPhases) + ", finalPhase=" + finalPhase + ", plansAllowedInBundle="
                 + plansAllowedInBundle + "]";
+    }
+
+    private DefaultPriceList findPriceListForPlan(final StandaloneCatalog catalog) {
+        for (PriceList cur : catalog.getPriceLists().getAllPriceLists()) {
+            for (Plan p : cur.getPlans()) {
+                if (p.getName().equals(name)) {
+                    return (DefaultPriceList) cur;
+                }
+            }
+        }
+        throw new IllegalStateException("Cannot extract pricelist for plan " + name);
     }
 }

subscription/src/main/java/org/killbill/billing/subscription/api/user/DefaultSubscriptionBase.java
@@ -569,7 +569,6 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
         EntitlementState nextState = null;
         String nextPlanName = null;
         String nextPhaseName = null;
-        String nextPriceListName = null;
 
         UUID prevEventId = null;
         DateTime prevCreatedDate = null;
@@ -620,12 +619,10 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
                             nextState = EntitlementState.ACTIVE;
                             nextPlanName = userEV.getEventPlan();
                             nextPhaseName = userEV.getEventPlanPhase();
-                            nextPriceListName = userEV.getPriceList();
                             break;
                         case CHANGE:
                             nextPlanName = userEV.getEventPlan();
                             nextPhaseName = userEV.getEventPlanPhase();
-                            nextPriceListName = userEV.getPriceList();
                             break;
                         case CANCEL:
                             nextState = EntitlementState.CANCELLED;
@@ -650,10 +647,7 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
 
             nextPlan = (nextPlanName != null) ? catalog.findPlan(nextPlanName, cur.getEffectiveDate(), getAlignStartDate()) : null;
             nextPhase = (nextPhaseName != null) ? catalog.findPhase(nextPhaseName, cur.getEffectiveDate(), getAlignStartDate()) : null;
-
-            // See issue https://github.com/killbill/killbill/issues/464
-            final DateTime catalogEffectiveDateForPriceList = transitions.isEmpty() ? cur.getEffectiveDate() : transitions.get(0).getEffectiveTransitionTime();
-            nextPriceList = (nextPriceListName != null) ? catalog.findPriceList(nextPriceListName, catalogEffectiveDateForPriceList) : null;
+            nextPriceList = (nextPlan != null) ? nextPlan.getPriceList() : null;
 
             final SubscriptionBaseTransitionData transition = new SubscriptionBaseTransitionData(
                     cur.getId(), id, bundleId, cur.getType(), apiEventType,

util/src/test/java/org/killbill/billing/mock/MockPlan.java
@@ -26,6 +26,7 @@ import org.killbill.billing.catalog.api.CatalogApiException;
 import org.killbill.billing.catalog.api.PhaseType;
 import org.killbill.billing.catalog.api.Plan;
 import org.killbill.billing.catalog.api.PlanPhase;
+import org.killbill.billing.catalog.api.PriceList;
 import org.killbill.billing.catalog.api.Product;
 
 public class MockPlan implements Plan {
@@ -49,6 +50,11 @@ public class MockPlan implements Plan {
     @Override
     public Product getProduct() {
         return product;
+    }
+
+    @Override
+    public PriceList getPriceList() {
+        throw new UnsupportedOperationException();
     }
 
     @Override

beatrix/src/test/resources/retiredCatalogs/WeaponsHireSmall-v1.xml
@@ -334,6 +334,8 @@
                 <plan>pistol-monthly</plan>
                 <plan>shotgun-monthly</plan>
                 <plan>shotgun-annual</plan>
+                <plan>laser-scope-monthly</plan>
+                <plan>extra-ammo-monthly</plan>
             </plans>
         </defaultPriceList>
         <childPriceList name="SpecialDiscount">

beatrix/src/test/resources/retiredCatalogs/WeaponsHireSmall-v2.xml
@@ -295,6 +295,8 @@
             <plans>
                 <plan>shotgun-monthly</plan>
                 <plan>shotgun-annual</plan>
+                <plan>laser-scope-monthly</plan>
+                <plan>extra-ammo-monthly</plan>
             </plans>
         </defaultPriceList>
         <childPriceList name="SpecialDiscount">

beatrix/src/test/resources/retiredCatalogs/WeaponsHireSmall-v3.xml
@@ -215,6 +215,9 @@
         <defaultPriceList name="DEFAULT">
             <plans>
                 <plan>shotgun-monthly</plan>
+                <plan>shotgun-annual</plan>
+                <plan>laser-scope-monthly</plan>
+                <plan>extra-ammo-monthly</plan>
             </plans>
         </defaultPriceList>
     </priceLists>

catalog/src/main/java/org/killbill/billing/catalog/StandaloneCatalog.java
@@ -281,8 +281,8 @@ public class StandaloneCatalog extends ValidatingConfig<StandaloneCatalog> imple
         for (final DefaultPlan p : plans) {
             p.initialize(catalog, sourceURI);
         }
-
     }
+
 
     //////////////////////////////////////////////////////////////////////////////
     //

catalog/src/main/java/org/killbill/billing/catalog/io/VersionedCatalogLoader.java
@@ -77,7 +77,7 @@ public class VersionedCatalogLoader implements CatalogLoader {
             }
             return result;
         } catch (Exception e) {
-            throw new CatalogApiException(ErrorCode.CAT_INVALID_DEFAULT, "Problem encountered loading catalog ", e);
+            throw new CatalogApiException(ErrorCode.CAT_INVALID_DEFAULT, "Problem encountered loading catalog: ", e.getMessage());
         }
     }
 

commit 134681dc7e281ea89cb5a4651c8423d694700c82

catalog: Fix regression introduced in 2c730de (catalog curcular dependency) and instead implement new method Catalog#findCatalogPlanEntry. See #464

catalog/src/main/java/org/killbill/billing/catalog/DefaultPlan.java
@@ -78,7 +78,7 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
     @XmlElement(required = false)
     private Integer plansAllowedInBundle = 1;
 
-    private PriceList priceList;
+    private String priceListName;
 
     public DefaultPlan() {
         initialPhases = new DefaultPlanPhase[0];
@@ -94,7 +94,7 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
             initialPhases[i] = newPhase;
         }
         this.finalPhase = new DefaultPlanPhase(this, in.getFinalPhase(), overrides[overrides.length - 1]);
-        this.priceList = in.getPriceList();
+        this.priceListName = in.getPriceListName();
     }
 
     @Override
@@ -113,8 +113,8 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
     }
 
     @Override
-    public PriceList getPriceList() {
-        return priceList;
+    public String getPriceListName() {
+        return priceListName;
     }
 
     @Override
@@ -185,7 +185,7 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
                 p.initialize(catalog, sourceURI);
             }
         }
-        this.priceList = findPriceListForPlan(catalog);
+        this.priceListName = findPriceListForPlan(catalog);
     }
 
     @Override
@@ -223,8 +223,8 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
         return this;
     }
 
-    public DefaultPlan setPriceList(final DefaultPriceList priceList) {
-        this.priceList = priceList;
+    public DefaultPlan setPriceListName(final String priceListName) {
+        this.priceListName = priceListName;
         return this;
     }
 
@@ -313,11 +313,11 @@ public class DefaultPlan extends ValidatingConfig<StandaloneCatalog> implements 
                + plansAllowedInBundle + "]";
     }
 
-    private DefaultPriceList findPriceListForPlan(final StandaloneCatalog catalog) {
+    private String findPriceListForPlan(final StandaloneCatalog catalog) {
         for (PriceList cur : catalog.getPriceLists().getAllPriceLists()) {
             for (Plan p : cur.getPlans()) {
                 if (p.getName().equals(name)) {
-                    return (DefaultPriceList) cur;
+                    return ((DefaultPriceList) cur).getName();
                 }
             }
         }

catalog/src/main/java/org/killbill/billing/catalog/VersionedCatalog.java
@@ -23,10 +23,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import javax.annotation.Nullable;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -149,9 +147,9 @@ public class VersionedCatalog extends ValidatingConfig<StandaloneCatalogWithPric
         }
     }
 
-    private Plan findPlan(final PlanRequestWrapper wrapper,
-                          final DateTime requestedDate,
-                          final DateTime subscriptionStartDate)
+    private CatalogPlanEntry findCatalogPlanEntry(final PlanRequestWrapper wrapper,
+                                                  final DateTime requestedDate,
+                                                  final DateTime subscriptionStartDate)
             throws CatalogApiException {
         final List<StandaloneCatalogWithPriceOverride> catalogs = versionsBeforeDate(requestedDate.toDate());
         if (catalogs.isEmpty()) {
@@ -174,12 +172,12 @@ public class VersionedCatalog extends ValidatingConfig<StandaloneCatalogWithPric
 
             final DateTime catalogEffectiveDate = CatalogDateHelper.toUTCDateTime(c.getEffectiveDate());
             if (!subscriptionStartDate.isBefore(catalogEffectiveDate)) { // Its a new subscription this plan always applies
-                return plan;
+                return new CatalogPlanEntry(c, plan);
             } else { //Its an existing subscription
                 if (plan.getEffectiveDateForExistingSubscriptions() != null) { //if it is null any change to this does not apply to existing subscriptions
                     final DateTime existingSubscriptionDate = CatalogDateHelper.toUTCDateTime(plan.getEffectiveDateForExistingSubscriptions());
                     if (requestedDate.isAfter(existingSubscriptionDate)) { // this plan is now applicable to existing subs
-                        return plan;
+                        return new CatalogPlanEntry(c, plan);
                     }
                 }
             }
@@ -187,6 +185,26 @@ public class VersionedCatalog extends ValidatingConfig<StandaloneCatalogWithPric
 
         throw new CatalogApiException(ErrorCode.CAT_NO_CATALOG_FOR_GIVEN_DATE, requestedDate.toDate().toString());
     }
+
+    private static class CatalogPlanEntry {
+
+        private final StaticCatalog staticCatalog;
+        private final Plan plan;
+
+        public CatalogPlanEntry(final StaticCatalog staticCatalog, final Plan plan) {
+            this.staticCatalog = staticCatalog;
+            this.plan = plan;
+        }
+
+        public StaticCatalog getStaticCatalog() {
+            return staticCatalog;
+        }
+
+        public Plan getPlan() {
+            return plan;
+        }
+    }
+
 
     public Clock getClock() {
         return clock;
@@ -284,7 +302,8 @@ public class VersionedCatalog extends ValidatingConfig<StandaloneCatalogWithPric
                          final DateTime requestedDate,
                          final DateTime subscriptionStartDate)
             throws CatalogApiException {
-        return findPlan(new PlanRequestWrapper(name), requestedDate, subscriptionStartDate);
+        final CatalogPlanEntry entry = findCatalogPlanEntry(new PlanRequestWrapper(name), requestedDate, subscriptionStartDate);
+        return entry.getPlan();
     }
 
     @Override
@@ -295,7 +314,8 @@ public class VersionedCatalog extends ValidatingConfig<StandaloneCatalogWithPric
                                  final DateTime requestedDate,
                                  final DateTime subscriptionStartDate)
             throws CatalogApiException {
-        return findPlan(new PlanRequestWrapper(productName, term, priceListName, overrides), requestedDate, subscriptionStartDate);
+        final CatalogPlanEntry entry =  findCatalogPlanEntry(new PlanRequestWrapper(productName, term, priceListName, overrides), requestedDate, subscriptionStartDate);
+        return entry.getPlan();
     }
 
     //
@@ -320,13 +340,23 @@ public class VersionedCatalog extends ValidatingConfig<StandaloneCatalogWithPric
     }
 
     //
-    // Find a price list
+    // Find a price list associated to a given subscription
     //
     @Override
+    public PriceList findPriceListForPlan(final String planName,
+                                          final DateTime requestedDate,
+                                          final DateTime subscriptionStartDate)
+            throws CatalogApiException {
+        final CatalogPlanEntry entry = findCatalogPlanEntry(new PlanRequestWrapper(planName), requestedDate, subscriptionStartDate);
+        return entry.getStaticCatalog().findCurrentPricelist(entry.getPlan().getPriceListName());
+    }
+
+
     public PriceList findPriceList(final String name, final DateTime requestedDate)
             throws CatalogApiException {
         return versionForDate(requestedDate).findCurrentPriceList(name);
     }
+
 
     //
     // Rules

entitlement/src/main/java/org/killbill/billing/entitlement/engine/core/EventsStreamBuilder.java
@@ -374,7 +374,7 @@ public class EventsStreamBuilder {
             productCategory = lastActiveProduct.getCategory();
             final PlanPhase lastActivePlanPhase = subscription.getLastActivePhase();
             billingPeriod = lastActivePlanPhase.getRecurring() != null ? lastActivePlanPhase.getRecurring().getBillingPeriod() : BillingPeriod.NO_BILLING_PERIOD;
-            priceListName = subscription.getLastActivePlan().getPriceList().getName();
+            priceListName = subscription.getLastActivePlan().getPriceListName();
             phaseType = subscription.getLastActivePhase().getPhaseType();
         }
         return new PlanPhaseSpecifier(lastActiveProductName,

pom.xml
@@ -21,7 +21,7 @@
     <parent>
         <artifactId>killbill-oss-parent</artifactId>
         <groupId>org.kill-bill.billing</groupId>
-        <version>0.111</version>
+        <version>0.112</version>
     </parent>
     <artifactId>killbill</artifactId>
     <version>0.17.1-SNAPSHOT</version>

subscription/src/main/java/org/killbill/billing/subscription/api/user/DefaultSubscriptionBase.java
@@ -664,7 +664,7 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
 
             nextPlan = (nextPlanName != null) ? catalog.findPlan(nextPlanName, cur.getEffectiveDate(), getAlignStartDate()) : null;
             nextPhase = (nextPhaseName != null) ? catalog.findPhase(nextPhaseName, cur.getEffectiveDate(), getAlignStartDate()) : null;
-            nextPriceList = (nextPlan != null) ? nextPlan.getPriceList() : null;
+            nextPriceList = (nextPlan != null) ? catalog.findPriceListForPlan(nextPlanName, cur.getEffectiveDate(), getAlignStartDate()) : null;
 
             final SubscriptionBaseTransitionData transition = new SubscriptionBaseTransitionData(
                     cur.getId(), id, bundleId, cur.getType(), apiEventType,

catalog/src/test/java/org/killbill/billing/catalog/MockCatalog.java
@@ -154,6 +154,11 @@ public class MockCatalog extends StandaloneCatalog implements Catalog {
     }
 
     @Override
+    public PriceList findPriceListForPlan(final String name, final DateTime requestedDate, final DateTime subscriptionStartDate) throws CatalogApiException {
+        return findCurrentPricelist(name);
+    }
+
+    @Override
     public BillingActionPolicy planChangePolicy(final PlanPhaseSpecifier from, final PlanSpecifier to, final DateTime requestedDate)
             throws CatalogApiException {
         return planChangePolicy(from, to);

util/src/test/java/org/killbill/billing/mock/MockPlan.java
@@ -53,7 +53,7 @@ public class MockPlan implements Plan {
     }
 
     @Override
-    public PriceList getPriceList() {
+    public String getPriceListName() {
         throw new UnsupportedOperationException();
     }
 