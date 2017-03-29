ffd1dbe81348ecf0eb46e0e50657e80daccd473b
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-03-29 04:07:24 -0700

    history: enhance a few existing tests to verify history tables
    This is in preparation of upcoming performance-related history and audit changes.
    
    See also https://github.com/killbill/killbill/issues/335.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- payment/src/test/java/org/killbill/billing/payment/core/TestPaymentMethodProcessorWithDB.java
+++ payment/src/test/java/org/killbill/billing/payment/core/TestPaymentMethodProcessorWithDB.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2014-2015 Groupon, Inc
- * Copyright 2014-2015 The Billing Project, LLC
+ * Copyright 2014-2017 Groupon, Inc
+ * Copyright 2014-2017 The Billing Project, LLC
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
@@ -17,12 +17,18 @@
 
 package org.killbill.billing.payment.core;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.killbill.billing.account.api.Account;
 import org.killbill.billing.payment.PaymentTestSuiteWithEmbeddedDB;
 import org.killbill.billing.payment.api.PaymentApiException;
+import org.killbill.billing.payment.api.PaymentMethod;
 import org.killbill.billing.payment.api.PluginProperty;
+import org.killbill.billing.payment.dao.PaymentMethodModelDao;
+import org.killbill.billing.payment.dao.PaymentMethodSqlDao;
+import org.killbill.billing.util.audit.ChangeType;
+import org.killbill.billing.util.dao.EntityHistoryModelDao;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -54,5 +60,44 @@ public class TestPaymentMethodProcessorWithDB extends PaymentTestSuiteWithEmbedd
 
         final Account accountById = accountApi.getAccountById(account.getId(), internalCallContext);
         Assert.assertEquals(accountById.getPaymentMethodId(), newPaymentMethod);
+    }
+
+    @Test(groups = "slow")
+    public void testDeletePaymentMethod() throws Exception {
+        final Account account = testHelper.createTestAccount("foo@bar.com", true);
+
+        final UUID paymentMethodId = paymentMethodProcessor.createOrGetExternalPaymentMethod("pmExternalKey", account, PLUGIN_PROPERTIES, callContext, internalCallContext);
+        final PaymentMethodModelDao paymentMethodModelDao = paymentDao.getPaymentMethod(paymentMethodId, internalCallContext);
+
+        final List<EntityHistoryModelDao<PaymentMethodModelDao, PaymentMethod>> history1 = getPaymentMethodHistory(paymentMethodModelDao.getRecordId());
+        Assert.assertEquals(history1.size(), 1);
+        Assert.assertEquals(history1.get(0).getChangeType(), ChangeType.INSERT);
+        Assert.assertEquals(history1.get(0).getEntity().getAccountRecordId(), paymentMethodModelDao.getAccountRecordId());
+        Assert.assertEquals(history1.get(0).getEntity().getTenantRecordId(), paymentMethodModelDao.getTenantRecordId());
+        Assert.assertEquals(history1.get(0).getEntity().getExternalKey(), paymentMethodModelDao.getExternalKey());
+        Assert.assertTrue(history1.get(0).getEntity().isActive());
+
+        paymentMethodProcessor.deletedPaymentMethod(account, paymentMethodId, true, true, ImmutableList.<PluginProperty>of(), callContext, internalCallContext);
+
+        final List<EntityHistoryModelDao<PaymentMethodModelDao, PaymentMethod>> history2 = getPaymentMethodHistory(paymentMethodModelDao.getRecordId());
+        Assert.assertEquals(history2.size(), 2);
+        Assert.assertEquals(history2.get(0).getChangeType(), ChangeType.INSERT);
+        Assert.assertEquals(history2.get(0).getEntity().getAccountRecordId(), paymentMethodModelDao.getAccountRecordId());
+        Assert.assertEquals(history2.get(0).getEntity().getTenantRecordId(), paymentMethodModelDao.getTenantRecordId());
+        Assert.assertEquals(history2.get(0).getEntity().getExternalKey(), paymentMethodModelDao.getExternalKey());
+        Assert.assertTrue(history2.get(0).getEntity().isActive());
+        // Note: it looks like we don't consider this as a DELETE, probably because we can un-delete such payment methods?
+        Assert.assertEquals(history2.get(1).getChangeType(), ChangeType.UPDATE);
+        Assert.assertEquals(history2.get(1).getEntity().getAccountRecordId(), paymentMethodModelDao.getAccountRecordId());
+        Assert.assertEquals(history2.get(1).getEntity().getTenantRecordId(), paymentMethodModelDao.getTenantRecordId());
+        Assert.assertEquals(history2.get(1).getEntity().getExternalKey(), paymentMethodModelDao.getExternalKey());
+        // Note: upon deletion, the recorded state is the same as before the delete
+        Assert.assertTrue(history2.get(1).getEntity().isActive());
+    }
+
+    private List<EntityHistoryModelDao<PaymentMethodModelDao, PaymentMethod>> getPaymentMethodHistory(final Long paymentMethodRecordId) {
+        // See https://github.com/killbill/killbill/issues/335
+        final PaymentMethodSqlDao paymentMethodSqlDao = dbi.onDemand(PaymentMethodSqlDao.class);
+        return paymentMethodSqlDao.getHistoryForTargetRecordId(paymentMethodRecordId, internalCallContext);
     }
 }
---
+++ util/src/main/java/org/killbill/billing/util/dao/EntityHistoryModelDaoMapper.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017 Groupon, Inc
+ * Copyright 2017 The Billing Project, LLC
+ *
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.killbill.billing.util.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.killbill.billing.util.audit.ChangeType;
+import org.killbill.billing.util.entity.Entity;
+import org.killbill.billing.util.entity.dao.EntityModelDao;
+import org.killbill.billing.util.entity.dao.EntityModelDaoBase;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+public class EntityHistoryModelDaoMapper<M extends EntityModelDao<E>, E extends Entity> extends MapperBase implements ResultSetMapper<EntityHistoryModelDao<M, E>> {
+
+    private final ResultSetMapper<M> entityMapper;
+
+    public EntityHistoryModelDaoMapper(final ResultSetMapper<M> entityMapper) {
+        this.entityMapper = entityMapper;
+    }
+
+    @Override
+    public EntityHistoryModelDao<M, E> map(final int index, final ResultSet r, final StatementContext ctx) throws SQLException {
+        final UUID id = getUUID(r, "id");
+        final long targetRecordId = r.getLong("target_record_id");
+        final String changeType = r.getString("change_type");
+        final DateTime createdDate = getDateTime(r, "created_date");
+
+        final M entityModelDao = entityMapper.map(index, r, ctx);
+
+        // Hack -- remove the id as it is the history id, not the entity id
+        ((EntityModelDaoBase) entityModelDao).setId(null);
+        // Hack -- similarly, populate the right record_id
+        ((EntityModelDaoBase) entityModelDao).setRecordId(targetRecordId);
+        // Hack -- account is special
+        if (entityModelDao.getAccountRecordId() == null) {
+            ((EntityModelDaoBase) entityModelDao).setAccountRecordId(targetRecordId);
+        }
+
+        return new EntityHistoryModelDao(id, entityModelDao, targetRecordId, ChangeType.valueOf(changeType), createdDate);
+    }
+}
