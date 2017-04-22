f5ec7de51ad2b35677ec1b117a4454d114b2ddeb
stephane brossier <sbrossier@groupon.com>
2017-04-21 19:36:03 -0700

    subscription, entitlement: Enlarge the meaning of getLastActivePlan|Product|Phase to also return information on PENDING subscription.

--- subscription/src/main/java/org/killbill/billing/subscription/api/user/DefaultSubscriptionBase.java
+++ subscription/src/main/java/org/killbill/billing/subscription/api/user/DefaultSubscriptionBase.java
@@ -18,20 +18,12 @@
 
 package org.killbill.billing.subscription.api.user;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
-
-import javax.annotation.Nullable;
-
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
-import org.killbill.billing.callcontext.InternalCallContext;
 import org.killbill.billing.callcontext.InternalTenantContext;
 import org.killbill.billing.catalog.api.BillingActionPolicy;
 import org.killbill.billing.catalog.api.BillingAlignment;
@@ -68,9 +60,14 @@ import org.killbill.clock.Clock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
 
 public class DefaultSubscriptionBase extends EntityBase implements SubscriptionBase {
 
@@ -309,6 +306,9 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
         if (getState() == EntitlementState.CANCELLED) {
             final SubscriptionBaseTransition data = getPreviousTransition();
             return data.getPreviousPlan().getProduct();
+        } else if (getState() == EntitlementState.PENDING) {
+            final SubscriptionBaseTransition data = getPendingTransition();
+            return data.getNextPlan().getProduct();
         } else {
             final Plan currentPlan = getCurrentPlan();
             // currentPlan can be null when playing with the clock (subscription created in the future)
@@ -321,6 +321,9 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
         if (getState() == EntitlementState.CANCELLED) {
             final SubscriptionBaseTransition data = getPreviousTransition();
             return data.getPreviousPriceList();
+        } else if (getState() == EntitlementState.PENDING) {
+            final SubscriptionBaseTransition data = getPendingTransition();
+            return data.getNextPriceList();
         } else {
             return getCurrentPriceList();
         }
@@ -331,6 +334,9 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
         if (getState() == EntitlementState.CANCELLED) {
             final SubscriptionBaseTransition data = getPreviousTransition();
             return data.getPreviousPlan().getProduct().getCategory();
+        } else if (getState() == EntitlementState.PENDING) {
+            final SubscriptionBaseTransition data = getPendingTransition();
+            return data.getNextPlan().getProduct().getCategory();
         } else {
             final Plan currentPlan = getCurrentPlan();
             // currentPlan can be null when playing with the clock (subscription created in the future)
@@ -343,6 +349,9 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
         if (getState() == EntitlementState.CANCELLED) {
             final SubscriptionBaseTransition data = getPreviousTransition();
             return data.getPreviousPlan();
+        } else if (getState() == EntitlementState.PENDING) {
+            final SubscriptionBaseTransition data = getPendingTransition();
+            return data.getNextPlan();
         } else {
             return getCurrentPlan();
         }
@@ -353,6 +362,9 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
         if (getState() == EntitlementState.CANCELLED) {
             final SubscriptionBaseTransition data = getPreviousTransition();
             return data.getPreviousPhase();
+        } else if (getState() == EntitlementState.PENDING) {
+            final SubscriptionBaseTransition data = getPendingTransition();
+            return data.getNextPhase();
         } else {
             return getCurrentPhase();
         }
@@ -363,6 +375,9 @@ public class DefaultSubscriptionBase extends EntityBase implements SubscriptionB
         if (getState() == EntitlementState.CANCELLED) {
             final SubscriptionBaseTransition data = getPreviousTransition();
             return data.getPreviousPlan().getRecurringBillingPeriod();
+        } else if (getState() == EntitlementState.PENDING) {
+            final SubscriptionBaseTransition data = getPendingTransition();
+            return data.getNextPlan().getRecurringBillingPeriod();
         } else {
             final Plan currentPlan = getCurrentPlan();
             // currentPlan can be null when playing with the clock (subscription created in the future)
