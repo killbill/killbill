killbill/killbill

commit 2c6acc87172147c1e3a342b38c567c29170004ce

Remove resource jersey config list, and setup all the resources by hand (making the TestResource conditional to the system property org.killbill.server.test.mode). See #763

profiles/killbill/src/main/java/org/killbill/billing/server/modules/KillbillServerModule.java
@@ -31,12 +31,14 @@ import org.killbill.billing.jaxrs.resources.AccountResource;
 import org.killbill.billing.jaxrs.resources.AdminResource;
 import org.killbill.billing.jaxrs.resources.BundleResource;
 import org.killbill.billing.jaxrs.resources.CatalogResource;
+import org.killbill.billing.jaxrs.resources.ComboPaymentResource;
 import org.killbill.billing.jaxrs.resources.CreditResource;
 import org.killbill.billing.jaxrs.resources.CustomFieldResource;
 import org.killbill.billing.jaxrs.resources.ExportResource;
 import org.killbill.billing.jaxrs.resources.InvoicePaymentResource;
 import org.killbill.billing.jaxrs.resources.InvoiceResource;
 import org.killbill.billing.jaxrs.resources.NodesInfoResource;
+import org.killbill.billing.jaxrs.resources.OverdueResource;
 import org.killbill.billing.jaxrs.resources.PaymentGatewayResource;
 import org.killbill.billing.jaxrs.resources.PaymentMethodResource;
 import org.killbill.billing.jaxrs.resources.PaymentResource;
@@ -182,29 +184,31 @@ public class KillbillServerModule extends KillbillPlatformModule {
 
     protected void configureResources() {
         bind(AccountResource.class).asEagerSingleton();
+        bind(AdminResource.class).asEagerSingleton();
         bind(BundleResource.class).asEagerSingleton();
         bind(CatalogResource.class).asEagerSingleton();
+        bind(ComboPaymentResource.class).asEagerSingleton();
         bind(CreditResource.class).asEagerSingleton();
         bind(CustomFieldResource.class).asEagerSingleton();
         bind(ExportResource.class).asEagerSingleton();
         bind(InvoicePaymentResource.class).asEagerSingleton();
         bind(InvoiceResource.class).asEagerSingleton();
-        bind(KillbillEventHandler.class).asEagerSingleton();
+        bind(NodesInfoResource.class).asEagerSingleton();
+        bind(OverdueResource.class).asEagerSingleton();
         bind(PaymentGatewayResource.class).asEagerSingleton();
         bind(PaymentMethodResource.class).asEagerSingleton();
         bind(PaymentResource.class).asEagerSingleton();
+        bind(PluginInfoResource.class).asEagerSingleton();
         bind(PluginResource.class).asEagerSingleton();
         bind(SecurityResource.class).asEagerSingleton();
         bind(SubscriptionResource.class).asEagerSingleton();
         bind(TagDefinitionResource.class).asEagerSingleton();
         bind(TagResource.class).asEagerSingleton();
         bind(TenantResource.class).asEagerSingleton();
-        bind(TestResource.class).asEagerSingleton();
         bind(TransactionResource.class).asEagerSingleton();
         bind(UsageResource.class).asEagerSingleton();
-        bind(AdminResource.class).asEagerSingleton();
-        bind(PluginInfoResource.class).asEagerSingleton();
-        bind(NodesInfoResource.class).asEagerSingleton();
+
+        bind(KillbillEventHandler.class).asEagerSingleton();
     }
 
     protected void configureFilters() {

profiles/killpay/src/main/java/org/killbill/billing/server/modules/KillpayServerModule.java
@@ -27,12 +27,17 @@ import org.killbill.billing.entitlement.glue.DefaultEntitlementModule;
 import org.killbill.billing.invoice.glue.DefaultInvoiceModule;
 import org.killbill.billing.jaxrs.glue.DefaultJaxrsModule;
 import org.killbill.billing.jaxrs.resources.AccountResource;
+import org.killbill.billing.jaxrs.resources.AdminResource;
+import org.killbill.billing.jaxrs.resources.ComboPaymentResource;
+import org.killbill.billing.jaxrs.resources.CreditResource;
 import org.killbill.billing.jaxrs.resources.CustomFieldResource;
 import org.killbill.billing.jaxrs.resources.ExportResource;
 import org.killbill.billing.jaxrs.resources.InvoicePaymentResource;
+import org.killbill.billing.jaxrs.resources.NodesInfoResource;
 import org.killbill.billing.jaxrs.resources.PaymentGatewayResource;
 import org.killbill.billing.jaxrs.resources.PaymentMethodResource;
 import org.killbill.billing.jaxrs.resources.PaymentResource;
+import org.killbill.billing.jaxrs.resources.PluginInfoResource;
 import org.killbill.billing.jaxrs.resources.PluginResource;
 import org.killbill.billing.jaxrs.resources.SecurityResource;
 import org.killbill.billing.jaxrs.resources.TagDefinitionResource;
@@ -115,19 +120,25 @@ public class KillpayServerModule extends KillbillServerModule {
     @Override
     protected void configureResources() {
         bind(AccountResource.class).asEagerSingleton();
+        bind(AdminResource.class).asEagerSingleton();
+        bind(ComboPaymentResource.class).asEagerSingleton();
+        bind(CreditResource.class).asEagerSingleton();
         bind(CustomFieldResource.class).asEagerSingleton();
         bind(ExportResource.class).asEagerSingleton();
-        bind(InvoicePaymentResource.class).asEagerSingleton();
+        bind(NodesInfoResource.class).asEagerSingleton();
         bind(KillbillEventHandler.class).asEagerSingleton();
         bind(PaymentGatewayResource.class).asEagerSingleton();
         bind(PaymentMethodResource.class).asEagerSingleton();
         bind(PaymentResource.class).asEagerSingleton();
         bind(PluginResource.class).asEagerSingleton();
+        bind(PluginInfoResource.class).asEagerSingleton();
         bind(SecurityResource.class).asEagerSingleton();
         bind(TagDefinitionResource.class).asEagerSingleton();
         bind(TagResource.class).asEagerSingleton();
         bind(TenantResource.class).asEagerSingleton();
         bind(TestResource.class).asEagerSingleton();
         bind(TransactionResource.class).asEagerSingleton();
+
+        bind(KillbillEventHandler.class).asEagerSingleton();
     }
 }

profiles/killbill/src/main/java/org/killbill/billing/server/listeners/KillbillGuiceListener.java
@@ -60,7 +60,8 @@ public class KillbillGuiceListener extends KillbillPlatformGuiceListener {
         // things like static resources, favicon, etc. are 404'ed)
         final BaseServerModuleBuilder builder = new BaseServerModuleBuilder().setJaxrsUriPattern("/" + SWAGGER_PATH + "|((/" + SWAGGER_PATH + "|" + JaxRsResourceBase.PREFIX + "|" + JaxRsResourceBase.PLUGINS_PATH + ")" + "/.*)")
                                                                              .addJaxrsResource("org.killbill.billing.jaxrs.mappers")
-                                                                             .addJaxrsResource("org.killbill.billing.jaxrs.resources")
+                                                                             // Dont' provide resources and instead add them automatically to control which one should be seen (e.g TestResource ony in testMode)
+                                                                             //.addJaxrsResource("org.killbill.billing.jaxrs.resources")
                                                                              // Swagger integration
                                                                              .addJaxrsResource("io.swagger.jaxrs.listing");
 

commit 7ffdeead59869e89f81caddb8374800b26377d68

Fix issue introduced in 2c6acc871. See #763

profiles/killbill/src/main/java/org/killbill/billing/server/modules/KillbillServerModule.java
@@ -187,7 +187,6 @@ public class KillbillServerModule extends KillbillPlatformModule {
         bind(AdminResource.class).asEagerSingleton();
         bind(BundleResource.class).asEagerSingleton();
         bind(CatalogResource.class).asEagerSingleton();
-        bind(ComboPaymentResource.class).asEagerSingleton();
         bind(CreditResource.class).asEagerSingleton();
         bind(CustomFieldResource.class).asEagerSingleton();
         bind(ExportResource.class).asEagerSingleton();

profiles/killpay/src/main/java/org/killbill/billing/server/modules/KillpayServerModule.java
@@ -28,11 +28,9 @@ import org.killbill.billing.invoice.glue.DefaultInvoiceModule;
 import org.killbill.billing.jaxrs.glue.DefaultJaxrsModule;
 import org.killbill.billing.jaxrs.resources.AccountResource;
 import org.killbill.billing.jaxrs.resources.AdminResource;
-import org.killbill.billing.jaxrs.resources.ComboPaymentResource;
 import org.killbill.billing.jaxrs.resources.CreditResource;
 import org.killbill.billing.jaxrs.resources.CustomFieldResource;
 import org.killbill.billing.jaxrs.resources.ExportResource;
-import org.killbill.billing.jaxrs.resources.InvoicePaymentResource;
 import org.killbill.billing.jaxrs.resources.NodesInfoResource;
 import org.killbill.billing.jaxrs.resources.PaymentGatewayResource;
 import org.killbill.billing.jaxrs.resources.PaymentMethodResource;
@@ -64,9 +62,9 @@ import org.killbill.billing.util.glue.ConfigModule;
 import org.killbill.billing.util.glue.CustomFieldModule;
 import org.killbill.billing.util.glue.ExportModule;
 import org.killbill.billing.util.glue.GlobalLockerModule;
-import org.killbill.billing.util.glue.NodesModule;
 import org.killbill.billing.util.glue.KillBillShiroAopModule;
 import org.killbill.billing.util.glue.KillbillApiAopModule;
+import org.killbill.billing.util.glue.NodesModule;
 import org.killbill.billing.util.glue.NonEntityDaoModule;
 import org.killbill.billing.util.glue.RecordIdModule;
 import org.killbill.billing.util.glue.SecurityModule;
@@ -121,7 +119,6 @@ public class KillpayServerModule extends KillbillServerModule {
     protected void configureResources() {
         bind(AccountResource.class).asEagerSingleton();
         bind(AdminResource.class).asEagerSingleton();
-        bind(ComboPaymentResource.class).asEagerSingleton();
         bind(CreditResource.class).asEagerSingleton();
         bind(CustomFieldResource.class).asEagerSingleton();
         bind(ExportResource.class).asEagerSingleton();