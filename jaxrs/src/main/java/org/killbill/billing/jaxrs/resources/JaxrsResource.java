f6a291f6455dc43095c1f01eb5f28b0980564bd9
stephane brossier <sbrossier@groupon.com>
2017-10-06 16:54:48 -0700

    jaxrs: Missing file for e0085a2b8d614

--- jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/JaxrsResource.java
+++ jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/JaxrsResource.java
@@ -263,6 +263,7 @@ public interface JaxrsResource {
     public static final String PAUSE = "pause";
     public static final String RESUME = "resume";
     public static final String BLOCK = "block";
+    public static final String RENAME_KEY = "renameKey";
 
     public static final String AUTHORIZATION = "authorization";
     public static final String CAPTURE = "capture";
