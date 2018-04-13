9b81efabfad251e4c040afd32977ef5d9ef85a70
stephane brossier <sbrossier@groupon.com>
2018-04-13 13:17:23 -0700

    profiles. Add swagger version. Fixes #932

--- profiles/killbill/src/main/java/org/killbill/billing/server/listeners/KillbillGuiceListener.java
+++ profiles/killbill/src/main/java/org/killbill/billing/server/listeners/KillbillGuiceListener.java
@@ -34,6 +34,7 @@ import org.killbill.billing.server.filters.ResponseCorsFilter;
 import org.killbill.billing.server.modules.KillbillServerModule;
 import org.killbill.billing.server.notifications.PushNotificationListener;
 import org.killbill.billing.server.security.TenantFilter;
+import org.killbill.billing.util.nodes.KillbillVersions;
 import org.killbill.bus.api.PersistentBus;
 import org.killbill.commons.skeleton.modules.BaseServerModuleBuilder;
 import org.slf4j.Logger;
@@ -149,6 +150,7 @@ public class KillbillGuiceListener extends KillbillPlatformGuiceListener {
         beanConfig.setContact("killbilling-users@googlegroups.com");
         beanConfig.setLicense("Apache License, Version 2.0");
         beanConfig.setLicenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
+        beanConfig.setVersion(KillbillVersions.getKillbillVersion());
         beanConfig.setScan(true);
     }
 
