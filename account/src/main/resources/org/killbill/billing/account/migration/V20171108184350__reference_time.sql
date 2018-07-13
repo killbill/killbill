bf51ba06dd652b2fc8227584657e98124a2d6773
stephane brossier <sbrossier@groupon.com>
2018-07-13 11:09:44 -0700

    Fix migration file V20171108184350__reference_time.sql to provide a DEFAULT value for reference time

--- account/src/main/resources/org/killbill/billing/account/migration/V20171108184350__reference_time.sql
+++ account/src/main/resources/org/killbill/billing/account/migration/V20171108184350__reference_time.sql
@@ -1,4 +1,4 @@
-alter table accounts add column reference_time datetime NOT NULL after payment_method_id;
-alter table account_history add column reference_time datetime NOT NULL after payment_method_id;
+alter table accounts add column reference_time datetime NOT NULL DEFAULT '1970-01-01 00:00:00' after payment_method_id;
+alter table account_history add column reference_time datetime NOT NULL DEFAULT '1970-01-01 00:00:00' after payment_method_id;
 update accounts set reference_time = created_date;
 update account_history set reference_time = created_date;
