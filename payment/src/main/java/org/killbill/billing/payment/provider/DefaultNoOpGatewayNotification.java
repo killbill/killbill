0d7ad165a67b86a55360f1de2a18bb391209ef4a
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-10-22 14:46:40 +0100

    payment: fix copyright header in DefaultNoOpGatewayNotification
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- payment/src/main/java/org/killbill/billing/payment/provider/DefaultNoOpGatewayNotification.java
+++ payment/src/main/java/org/killbill/billing/payment/provider/DefaultNoOpGatewayNotification.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2015 Groupon, Inc
- * Copyright 2015 The Billing Project, LLC
+ * Copyright 2015-2018 Groupon, Inc
+ * Copyright 2015-2018 The Billing Project, LLC
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
