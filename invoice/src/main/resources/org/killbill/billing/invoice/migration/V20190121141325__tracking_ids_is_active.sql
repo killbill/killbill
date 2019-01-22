2cd947a890dec2d126422d05fb3ec467e43bba4f
stephane brossier <sbrossier@groupon.com>
2019-01-21 17:26:22 -0800

    invoice: Fix migration file to include a non null default date

--- invoice/src/main/resources/org/killbill/billing/invoice/migration/V20190121141325__tracking_ids_is_active.sql
+++ invoice/src/main/resources/org/killbill/billing/invoice/migration/V20190121141325__tracking_ids_is_active.sql
@@ -1,4 +1,4 @@
 alter table invoice_tracking_ids add column is_active boolean default true after record_date;
 alter table invoice_tracking_ids add column updated_by varchar(50) NOT NULL after created_date;
-alter table invoice_tracking_ids add column updated_date datetime NOT NULL after updated_by;
+alter table invoice_tracking_ids add column updated_date datetime NOT NULL DEFAULT '1970-01-01 00:00:00' after updated_by;
 create index invoice_tracking_invoice_id_idx on invoice_tracking_ids(tenant_record_id, invoice_id);
