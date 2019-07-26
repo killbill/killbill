75cfe4e3a128f1ee09dfb6ada6521a06106f12f9
stephane brossier <sbrossier@groupon.com>
2019-07-26 10:05:51 -0700

    jaxrs: Remove unused required argument for InvoiceResource#createInstantPayment. See #1169

--- jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/InvoiceResource.java
+++ jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/InvoiceResource.java
@@ -669,9 +669,7 @@ public class InvoiceResource extends JaxRsResourceBase {
                                          @javax.ws.rs.core.Context final HttpServletRequest request,
                                          @javax.ws.rs.core.Context final UriInfo uriInfo) throws AccountApiException, PaymentApiException {
         verifyNonNullOrEmpty(payment, "InvoicePaymentJson body should be specified");
-        verifyNonNullOrEmpty(payment.getAccountId(), "InvoicePaymentJson accountId needs to be set",
-                             payment.getTargetInvoiceId(), "InvoicePaymentJson targetInvoiceId needs to be set",
-                             payment.getPurchasedAmount(), "InvoicePaymentJson purchasedAmount needs to be set");
+        verifyNonNullOrEmpty(payment.getAccountId(), "InvoicePaymentJson accountId needs to be set");
         Preconditions.checkArgument(!externalPayment || payment.getPaymentMethodId() == null, "InvoicePaymentJson should not contain a paymentMethodId when this is an external payment");
 
         final Iterable<PluginProperty> pluginProperties = extractPluginProperties(pluginPropertiesString);
