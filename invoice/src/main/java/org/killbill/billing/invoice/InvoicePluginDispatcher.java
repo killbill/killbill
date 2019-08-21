27230a9bf8af5ec2a18069f44822426554cedb08
stephane brossier <sbrossier@groupon.com>
2019-08-21 11:35:36 -0700

    invoice: Allow invoice plugin items to be passed with a null ID (as advertised in our doc). See #1182

--- invoice/src/main/java/org/killbill/billing/invoice/InvoicePluginDispatcher.java
+++ invoice/src/main/java/org/killbill/billing/invoice/InvoicePluginDispatcher.java
@@ -46,6 +46,7 @@ import org.killbill.billing.invoice.plugin.api.InvoicePluginApi;
 import org.killbill.billing.invoice.plugin.api.PriorInvoiceResult;
 import org.killbill.billing.osgi.api.OSGIServiceRegistration;
 import org.killbill.billing.payment.api.PluginProperty;
+import org.killbill.billing.util.UUIDs;
 import org.killbill.billing.util.callcontext.CallContext;
 import org.killbill.billing.util.config.definition.InvoiceConfig;
 import org.slf4j.Logger;
@@ -225,7 +226,9 @@ public class InvoicePluginDispatcher {
 
         final UUID invoiceId = MoreObjects.firstNonNull(mutableField("invoiceId", existingItem != null ? existingItem.getInvoiceId() : null, additionalInvoiceItem.getInvoiceId(), invoicePlugin),
                                                         originalInvoice.getId());
-        return new InvoiceItemCatalogBase(additionalInvoiceItem.getId(),
+
+        final UUID additionalInvoiceId = MoreObjects.firstNonNull(additionalInvoiceItem.getId(), UUIDs.randomUUID());
+        return new InvoiceItemCatalogBase(additionalInvoiceId,
                                           mutableField("createdDate", existingItem != null ? existingItem.getCreatedDate() : null, additionalInvoiceItem.getCreatedDate(), invoicePlugin),
                                           invoiceId,
                                           immutableField("accountId", existingItem, existingItem != null ? existingItem.getAccountId() : null, additionalInvoiceItem.getAccountId(), invoicePlugin),
