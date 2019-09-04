55c32b1e0d42d9a930e6b5c9b0ae79a4bea2d580
Pierre-Alexandre Meyer <pierre@mouraf.org>
2019-09-04 08:00:53 +0100

    entitlement: subscription: add logging around invalid specifiers
    The ErrorCode SUB_CREATE_INVALID_ENTITLEMENT_SPECIFIER doesn't
    allow for an error message to be passed. Add some logging to help
    the user understand the issue.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- entitlement/src/main/java/org/killbill/billing/entitlement/api/DefaultEntitlementApi.java
+++ entitlement/src/main/java/org/killbill/billing/entitlement/api/DefaultEntitlementApi.java
@@ -454,11 +454,13 @@ public class DefaultEntitlementApi extends DefaultEntitlementApiBase implements 
 
     private BaseEntitlementWithAddOnsSpecifier getFirstBaseEntitlementWithAddOnsSpecifier(final Iterable<BaseEntitlementWithAddOnsSpecifier> baseEntitlementWithAddOnsSpecifiers) throws SubscriptionBaseApiException {
         if (baseEntitlementWithAddOnsSpecifiers == null) {
+            log.warn("getFirstBaseEntitlementWithAddOnsSpecifier: baseEntitlementWithAddOnsSpecifiers is null");
             throw new SubscriptionBaseApiException(ErrorCode.SUB_CREATE_INVALID_ENTITLEMENT_SPECIFIER);
         }
 
         final Iterator<BaseEntitlementWithAddOnsSpecifier> iterator = baseEntitlementWithAddOnsSpecifiers.iterator();
         if (!iterator.hasNext()) {
+            log.warn("getFirstBaseEntitlementWithAddOnsSpecifier: baseEntitlementWithAddOnsSpecifiers is empty");
             throw new SubscriptionBaseApiException(ErrorCode.SUB_CREATE_INVALID_ENTITLEMENT_SPECIFIER);
         }
 
