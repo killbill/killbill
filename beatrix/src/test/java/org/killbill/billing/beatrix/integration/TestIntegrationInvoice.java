killbill/killbill

commit beca83bfe073b8fa14a4a248da0a1d1487db84fc

subscription: Change SubscriptionBillingEvent#Plan semantics for cancelled transition. See #1191
Previous code would compute such Plan to be the previous non null Plan which created an asymetric
meaning of what this Plan really mean and how it is compute. Instead we simply return null and need to
update the rest of the code such as junction (BillingEvent) and invoice to correctly understand this.

In addition we expose SubscriptionBillingEvent#getCatalogEffectiveDate which is required (when Plan is null)
and which should provide what we need to implement #1191 (as each BillingEvent now comes with its catalog version).

invoice/src/main/java/org/killbill/billing/invoice/generator/FixedAndRecurringInvoiceItemGenerator.java
@@ -38,6 +38,7 @@ import org.killbill.billing.catalog.api.BillingPeriod;
 import org.killbill.billing.catalog.api.CatalogApiException;
 import org.killbill.billing.catalog.api.Currency;
 import org.killbill.billing.catalog.api.PhaseType;
+import org.killbill.billing.catalog.api.Plan;
 import org.killbill.billing.invoice.api.Invoice;
 import org.killbill.billing.invoice.api.InvoiceApiException;
 import org.killbill.billing.invoice.api.InvoiceItem;
@@ -59,6 +60,7 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.Multimap;
@@ -142,11 +144,11 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
             if (!events.getSubscriptionIdsWithAutoInvoiceOff().
                     contains(thisEvent.getSubscriptionId())) { // don't consider events for subscriptions that have auto_invoice_off
                 final BillingEvent adjustedNextEvent = (thisEvent.getSubscriptionId() == nextEvent.getSubscriptionId()) ? nextEvent : null;
-                final List<InvoiceItem> newProposedItems = processRecurringEvent(invoiceId, accountId, thisEvent, adjustedNextEvent, targetDate, currency, invoiceItemGeneratorLogger, thisEvent.getPlan().getRecurringBillingMode(), perSubscriptionFutureNotificationDate, internalCallContext);
+                final List<InvoiceItem> newProposedItems = processRecurringEvent(invoiceId, accountId, thisEvent, adjustedNextEvent, targetDate, currency, invoiceItemGeneratorLogger, perSubscriptionFutureNotificationDate, internalCallContext);
                 proposedItems.addAll(newProposedItems);
             }
         }
-        final List<InvoiceItem> newProposedItems = processRecurringEvent(invoiceId, accountId, nextEvent, null, targetDate, currency, invoiceItemGeneratorLogger, nextEvent.getPlan().getRecurringBillingMode(), perSubscriptionFutureNotificationDate, internalCallContext);
+        final List<InvoiceItem> newProposedItems = processRecurringEvent(invoiceId, accountId, nextEvent, null, targetDate, currency, invoiceItemGeneratorLogger, perSubscriptionFutureNotificationDate, internalCallContext);
 
         proposedItems.addAll(newProposedItems);
 
@@ -202,21 +204,27 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
     // Turn a set of events into a list of invoice items. Note that the dates on the invoice items will be rounded (granularity of a day)
     private List<InvoiceItem> processRecurringEvent(final UUID invoiceId, final UUID accountId, final BillingEvent thisEvent, @Nullable final BillingEvent nextEvent,
                                                     final LocalDate targetDate, final Currency currency,
-                                                    final InvoiceItemGeneratorLogger invoiceItemGeneratorLogger, final BillingMode billingMode,
+                                                    final InvoiceItemGeneratorLogger invoiceItemGeneratorLogger,
                                                     final Map<UUID, SubscriptionFutureNotificationDates> perSubscriptionFutureNotificationDate,
                                                     final InternalCallContext internalCallContext) throws InvoiceApiException {
 
         try {
             final List<InvoiceItem> items = new ArrayList<InvoiceItem>();
 
-            // For FIXEDTERM phases we need to stop when the specified duration has been reached
-            final LocalDate maxEndDate = thisEvent.getPlanPhase().getPhaseType() == PhaseType.FIXEDTERM ?
-                                         thisEvent.getPlanPhase().getDuration().addToLocalDate(internalCallContext.toLocalDate(thisEvent.getEffectiveDate())) :
-                                         null;
 
             // Handle recurring items
             final BillingPeriod billingPeriod = thisEvent.getBillingPeriod();
             if (billingPeriod != BillingPeriod.NO_BILLING_PERIOD) {
+
+                final Plan currentPlan = thisEvent.getPlan();
+                Preconditions.checkNotNull(currentPlan, String.format("Unexpected null Plan name event = %s", thisEvent));
+
+                // For FIXEDTERM phases we need to stop when the specified duration has been reached
+                final LocalDate maxEndDate = thisEvent.getPlanPhase().getPhaseType() == PhaseType.FIXEDTERM ?
+                                             thisEvent.getPlanPhase().getDuration().addToLocalDate(internalCallContext.toLocalDate(thisEvent.getEffectiveDate())) :
+                                             null;
+
+                final BillingMode recurringBillingMode = currentPlan.getRecurringBillingMode();
                 final LocalDate startDate = internalCallContext.toLocalDate(thisEvent.getEffectiveDate());
 
                 if (!startDate.isAfter(targetDate)) {
@@ -226,7 +234,7 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
 
                     final RecurringInvoiceItemDataWithNextBillingCycleDate itemDataWithNextBillingCycleDate;
                     try {
-                        itemDataWithNextBillingCycleDate = generateInvoiceItemData(startDate, endDate, targetDate, billCycleDayLocal, billingPeriod, billingMode);
+                        itemDataWithNextBillingCycleDate = generateInvoiceItemData(startDate, endDate, targetDate, billCycleDayLocal, billingPeriod, recurringBillingMode);
                     } catch (final InvalidDateSequenceException e) {
                         throw new InvoiceApiException(ErrorCode.INVOICE_INVALID_DATE_SEQUENCE, startDate, endDate, targetDate);
                     }
@@ -244,8 +252,8 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
                                                                                                 accountId,
                                                                                                 thisEvent.getBundleId(),
                                                                                                 thisEvent.getSubscriptionId(),
-                                                                                                thisEvent.getPlan().getProduct().getName(),
-                                                                                                thisEvent.getPlan().getName(),
+                                                                                                currentPlan.getProduct().getName(),
+                                                                                                currentPlan.getName(),
                                                                                                 thisEvent.getPlanPhase().getName(),
                                                                                                 itemDatum.getStartDate(),
                                                                                                 itemDatum.getEndDate(),
@@ -253,7 +261,7 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
                             items.add(recurringItem);
                         }
                     }
-                    updatePerSubscriptionNextNotificationDate(thisEvent.getSubscriptionId(), itemDataWithNextBillingCycleDate.getNextBillingCycleDate(), items, billingMode,
+                    updatePerSubscriptionNextNotificationDate(thisEvent.getSubscriptionId(), itemDataWithNextBillingCycleDate.getNextBillingCycleDate(), items, recurringBillingMode,
                                                               perSubscriptionFutureNotificationDate);
                 }
             }
@@ -404,9 +412,13 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
             final BigDecimal fixedPrice = thisEvent.getFixedPrice();
 
             if (fixedPrice != null) {
+
+                final Plan currentPlan = thisEvent.getPlan();
+                Preconditions.checkNotNull(currentPlan, String.format("Unexpected null Plan name event = %s", thisEvent));
+
                 final FixedPriceInvoiceItem fixedPriceInvoiceItem = new FixedPriceInvoiceItem(invoiceId, accountId, thisEvent.getBundleId(),
                                                                                               thisEvent.getSubscriptionId(),
-                                                                                              thisEvent.getPlan().getProduct().getName(), thisEvent.getPlan().getName(), thisEvent.getPlanPhase().getName(),
+                                                                                              currentPlan.getProduct().getName(), currentPlan.getName(), thisEvent.getPlanPhase().getName(),
                                                                                               roundedStartDate, fixedPrice, currency);
 
                 // For debugging purposes

api/src/main/java/org/killbill/billing/subscription/api/user/SubscriptionBillingEvent.java
@@ -36,4 +36,6 @@ public interface SubscriptionBillingEvent extends Comparable<SubscriptionBilling
 
     Integer getBcdLocal();
 
+    DateTime getCatalogEffectiveDate();
+
 }

junction/src/main/java/org/killbill/billing/junction/plumbing/billing/DefaultBillingEvent.java
@@ -79,7 +79,7 @@ public class DefaultBillingEvent implements BillingEvent {
         this.plan = inputEvent.getPlan();
         this.planPhase = inputEvent.getPlanPhase();
 
-        this.catalogEffectiveDate = new DateTime(plan.getCatalog().getEffectiveDate());
+        this.catalogEffectiveDate = inputEvent.getCatalogEffectiveDate();
 
         this.currency = currency;
         this.billCycleDayLocal = billCycleDayLocal;
@@ -91,7 +91,7 @@ public class DefaultBillingEvent implements BillingEvent {
         // All those attributes are computed once for the billing and will be applied through all transitions
         // issued from this billing events -- e.g monthly recurring invoicing
         //
-        this.billingPeriod = computeRecurringBillingPeriod(planPhase);
+        this.billingPeriod = computeRecurringBillingPeriod(isCancelledOrBlocked, planPhase);
         this.fixedPrice = computeFixedPrice(isCancelledOrBlocked, planPhase, currency, type);
         this.recurringPrice = computeRecurringPrice(isCancelledOrBlocked, planPhase, currency);
         this.usages = computeUsages(isCancelledOrBlocked, planPhase);
@@ -229,7 +229,10 @@ public class DefaultBillingEvent implements BillingEvent {
         return (effectivePlanPhase.getRecurring() != null && effectivePlanPhase.getRecurring().getRecurringPrice() != null) ? effectivePlanPhase.getRecurring().getRecurringPrice().getPrice(currency) : null;
     }
 
-    private static BillingPeriod computeRecurringBillingPeriod(final PlanPhase effectivePlanPhase) {
+    private static BillingPeriod computeRecurringBillingPeriod(final boolean isCancelledOrBlocked, final PlanPhase effectivePlanPhase) {
+        if (isCancelledOrBlocked) {
+            return BillingPeriod.NO_BILLING_PERIOD;
+        }
         return effectivePlanPhase.getRecurring() != null ? effectivePlanPhase.getRecurring().getBillingPeriod() : BillingPeriod.NO_BILLING_PERIOD;
     }
 
@@ -375,7 +378,9 @@ public class DefaultBillingEvent implements BillingEvent {
         sb.append("DefaultBillingEvent");
         sb.append("{type=").append(type);
         sb.append(", effectiveDate=").append(effectiveDate);
-        sb.append(", planPhaseName=").append(planPhase.getName());
+        if (planPhase != null) {
+            sb.append(", planPhaseName=").append(planPhase.getName());
+        }
         sb.append(", subscriptionId=").append(subscriptionId);
         sb.append(", totalOrdering=").append(totalOrdering);
         sb.append('}');

junction/src/main/java/org/killbill/billing/junction/plumbing/billing/DefaultInternalBillingApi.java
@@ -39,7 +39,6 @@ import org.killbill.billing.catalog.api.BillingAlignment;
 import org.killbill.billing.catalog.api.CatalogApiException;
 import org.killbill.billing.catalog.api.CatalogInternalApi;
 import org.killbill.billing.catalog.api.PlanPhaseSpecifier;
-import org.killbill.billing.catalog.api.StaticCatalog;
 import org.killbill.billing.catalog.api.VersionedCatalog;
 import org.killbill.billing.entitlement.api.SubscriptionEventType;
 import org.killbill.billing.invoice.api.DryRunArguments;
@@ -285,19 +284,25 @@ public class DefaultInternalBillingApi implements BillingInternalApi {
             }
 
             Integer overridenBCD = null;
+            int bcdLocal = 0;
+            BillingAlignment alignment = null;
             for (final SubscriptionBillingEvent transition : billingTransitions) {
-                final PlanPhaseSpecifier spec = new PlanPhaseSpecifier(transition.getPlan().getName(), transition.getPlanPhase().getPhaseType());
-                final BillingAlignment alignment = subscription.getBillingAlignment(spec, transition.getEffectiveDate(), catalog);
 
-                //
-                // A BCD_CHANGE transition defines a new billCycleDayLocal for the subscription and this overrides whatever computation
-                // occurs below (which is based on billing alignment policy). Also multiple of those BCD_CHANGE transitions could occur,
-                // to define different intervals with different billing cycle days.
-                //
-                overridenBCD = transition.getBcdLocal() != null ? transition.getBcdLocal() : overridenBCD;
-                final int bcdLocal = overridenBCD != null ?
-                                     overridenBCD :
-                                     calculateBcdForTransition(alignment, bcdCache, baseSubscription, subscription, currentAccountBCD, context);
+                if (transition.getType() != SubscriptionBaseTransitionType.CANCEL) {
+                    final PlanPhaseSpecifier spec = new PlanPhaseSpecifier(transition.getPlan().getName(), transition.getPlanPhase().getPhaseType());
+                    alignment = subscription.getBillingAlignment(spec, transition.getEffectiveDate(), catalog);
+
+                    //
+                    // A BCD_CHANGE transition defines a new billCycleDayLocal for the subscription and this overrides whatever computation
+                    // occurs below (which is based on billing alignment policy). Also multiple of those BCD_CHANGE transitions could occur,
+                    // to define different intervals with different billing cycle days.
+                    //
+                    overridenBCD = transition.getBcdLocal() != null ? transition.getBcdLocal() : overridenBCD;
+                    bcdLocal = overridenBCD != null ?
+                               overridenBCD :
+                               calculateBcdForTransition(alignment, bcdCache, baseSubscription, subscription, currentAccountBCD, context);
+
+                }
 
                 final BillingEvent event = new DefaultBillingEvent(transition, subscription, bcdLocal, alignment, account.getCurrency());
                 result.add(event);

junction/src/test/java/org/killbill/billing/junction/plumbing/billing/TestBillingApi.java
@@ -58,6 +58,7 @@ import org.killbill.billing.subscription.api.user.DefaultSubscriptionBillingEven
 import org.killbill.billing.subscription.api.user.SubscriptionBaseApiException;
 import org.killbill.billing.subscription.api.user.SubscriptionBaseBundle;
 import org.killbill.billing.util.api.TagApiException;
+import org.killbill.billing.util.catalog.CatalogDateHelper;
 import org.killbill.billing.util.tag.ControlTagType;
 import org.killbill.billing.util.tag.dao.MockTagDao;
 import org.mockito.Mockito;
@@ -300,11 +301,12 @@ public class TestBillingApi extends JunctionTestSuiteNoDB {
         final EffectiveSubscriptionInternalEvent t = new MockEffectiveSubscriptionEvent(
                 eventId, subId, bunId, bunKey, then, now, null, null, null, null, null, EntitlementState.ACTIVE,
                 nextPlan.getName(), nextPhase.getName(),
-                nextPlan.getPriceListName(), null, 1L,
+                nextPlan.getPriceList().getName(), null, 1L,
                 SubscriptionBaseTransitionType.CREATE, 1, null, 1L, 2L, null);
 
         effectiveSubscriptionTransitions.add(t);
-        billingTransitions.add(new DefaultSubscriptionBillingEvent(SubscriptionBaseTransitionType.CREATE, nextPlan, nextPhase, now, 1L, null));
+        billingTransitions.add(new DefaultSubscriptionBillingEvent(SubscriptionBaseTransitionType.CREATE, nextPlan, nextPhase, now, 1L, null,
+                                                                   CatalogDateHelper.toUTCDateTime(nextPlan.getCatalog().getEffectiveDate())));
 
         return now;
     }

commit 492c2338c707028247b9ff4a878dbdd2d7d1008f

Fix invoice regression introduced in beca83bfe073.
The change introduced in https://github.com/killbill/killbill/commit/beca83bfe073b8fa14a4a248da0a1d1487db84fc#diff-6bbd5e696930ee20c12f24087244c833R234
is actually correct, i.e the recurring billing associated with an event marking cancelation or pause should be set to `NO_BILLING_PERIOD`.

However, our invoicing code contains another bug where we keep adding future notifications for canceled subscriptions. Prior this change,
this was not visible as the notification was set in the future but with the change from commit beca83bfe073, the notification becomes in the past
and what was previously an unused future notification now leads to infinite loops.

The fix is to remove future notifications when there is nothing to invoice in the future.

invoice/src/main/java/org/killbill/billing/invoice/generator/FixedAndRecurringInvoiceItemGenerator.java
@@ -213,12 +213,15 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
     private List<InvoiceItem> processRecurringEvent(final UUID invoiceId, final UUID accountId, final BillingEvent thisEvent, @Nullable final BillingEvent nextEvent,
                                                     final LocalDate targetDate, final Currency currency,
                                                     final InvoiceItemGeneratorLogger invoiceItemGeneratorLogger,
-                                                    final Map<UUID, SubscriptionFutureNotificationDates> perSubscriptionFutureNotificationDate,
+                                                    final Map<UUID, SubscriptionFutureNotificationDates> perSubscriptionFutureNotificationDates,
                                                     final InternalCallContext internalCallContext) throws InvoiceApiException {
 
         try {
             final List<InvoiceItem> items = new ArrayList<InvoiceItem>();
-
+            final LocalDate thisEventEffectiveDate = internalCallContext.toLocalDate(thisEvent.getEffectiveDate());
+            if (thisEventEffectiveDate.compareTo(targetDate) > 0) {
+                return items;
+            }
 
             // Handle recurring items
             final BillingPeriod billingPeriod = thisEvent.getBillingPeriod();
@@ -229,11 +232,11 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
 
                 // For FIXEDTERM phases we need to stop when the specified duration has been reached
                 final LocalDate maxEndDate = thisEvent.getPlanPhase().getPhaseType() == PhaseType.FIXEDTERM ?
-                                             thisEvent.getPlanPhase().getDuration().addToLocalDate(internalCallContext.toLocalDate(thisEvent.getEffectiveDate())) :
+                                             thisEvent.getPlanPhase().getDuration().addToLocalDate(thisEventEffectiveDate) :
                                              null;
 
                 final BillingMode recurringBillingMode = currentPlan.getRecurringBillingMode();
-                final LocalDate startDate = internalCallContext.toLocalDate(thisEvent.getEffectiveDate());
+                final LocalDate startDate = thisEventEffectiveDate;
 
                 if (!startDate.isAfter(targetDate)) {
                     final LocalDate endDate = (nextEvent == null) ? null : internalCallContext.toLocalDate(nextEvent.getEffectiveDate());
@@ -272,11 +275,16 @@ public class FixedAndRecurringInvoiceItemGenerator extends InvoiceItemGenerator 
                         }
                     }
                     updatePerSubscriptionNextNotificationDate(thisEvent.getSubscriptionId(), itemDataWithNextBillingCycleDate.getNextBillingCycleDate(), items, recurringBillingMode,
-                                                              perSubscriptionFutureNotificationDate);
+                                                              perSubscriptionFutureNotificationDates);
+                }
+            } else {
+                final SubscriptionFutureNotificationDates futureNotificationDates = perSubscriptionFutureNotificationDates.get(thisEvent.getSubscriptionId());
+                if (futureNotificationDates != null) {
+                    futureNotificationDates.clearNextRecurringDate();
                 }
             }
 
-            // For debugging purposes
+                // For debugging purposes
             invoiceItemGeneratorLogger.append(thisEvent, items);
 
             return items;

invoice/src/main/java/org/killbill/billing/invoice/generator/InvoiceWithMetadata.java
@@ -194,6 +194,10 @@ public class InvoiceWithMetadata {
             }
         }
 
+        public void clearNextRecurringDate() {
+            nextRecurringDate = null;
+        }
+
         public LocalDate getNextRecurringDate() {
             return nextRecurringDate;
         }

beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestIntegrationInvoice.java
@@ -30,6 +30,7 @@ import org.killbill.billing.account.api.Account;
 import org.killbill.billing.api.TestApiListener.NextEvent;
 import org.killbill.billing.beatrix.util.InvoiceChecker.ExpectedInvoiceItemCheck;
 import org.killbill.billing.catalog.DefaultPlanPhasePriceOverride;
+import org.killbill.billing.catalog.api.BillingActionPolicy;
 import org.killbill.billing.catalog.api.BillingPeriod;
 import org.killbill.billing.catalog.api.Currency;
 import org.killbill.billing.catalog.api.PlanPhasePriceOverride;
@@ -39,6 +40,7 @@ import org.killbill.billing.catalog.api.UsagePriceOverride;
 import org.killbill.billing.entitlement.api.DefaultEntitlement;
 import org.killbill.billing.entitlement.api.DefaultEntitlementSpecifier;
 import org.killbill.billing.entitlement.api.Entitlement;
+import org.killbill.billing.entitlement.api.Entitlement.EntitlementActionPolicy;
 import org.killbill.billing.invoice.api.Invoice;
 import org.killbill.billing.invoice.api.InvoiceItem;
 import org.killbill.billing.invoice.api.InvoiceItemType;
@@ -55,6 +57,54 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class TestIntegrationInvoice extends TestIntegrationBase {
+
+    @Test(groups = "slow")
+    public void testWithInArrearSubscription() throws Exception {
+        final DateTime initialCreationDate = new DateTime(2020, 1, 1, 0, 0, 0, 0, testTimeZone);
+        clock.setTime(initialCreationDate);
+
+        final Account account = createAccountWithNonOsgiPaymentMethod(getAccountData(1));
+
+        final PlanPhaseSpecifier spec = new PlanPhaseSpecifier("blowdart-in-arrear-monthly-notrial", null);
+
+        busHandler.pushExpectedEvents(NextEvent.CREATE, NextEvent.BLOCK, NextEvent.NULL_INVOICE);
+        final UUID entitlementId = entitlementApi.createBaseEntitlement(account.getId(),  new DefaultEntitlementSpecifier(spec, null, null, null), "bundleExternalKey", null, null, false, true, ImmutableList.<PluginProperty>of(), callContext);
+        assertListenerStatus();
+        final Entitlement entitlement = entitlementApi.getEntitlementForId(entitlementId, callContext);
+
+        // 2020-02-01
+        busHandler.pushExpectedEvents(NextEvent.INVOICE, NextEvent.INVOICE_PAYMENT, NextEvent.PAYMENT);
+        clock.addMonths(1);
+        assertListenerStatus();
+
+        invoiceChecker.checkInvoice(account.getId(), 1, callContext,
+                                    new ExpectedInvoiceItemCheck(new LocalDate(2020, 1, 1), new LocalDate(2020, 2, 1), InvoiceItemType.RECURRING, new BigDecimal("100.00")));
+
+
+        // 2020-03-01
+        busHandler.pushExpectedEvents(NextEvent.INVOICE, NextEvent.INVOICE_PAYMENT, NextEvent.PAYMENT);
+        clock.addMonths(1);
+        assertListenerStatus();
+
+        invoiceChecker.checkInvoice(account.getId(), 2, callContext,
+                                    new ExpectedInvoiceItemCheck(new LocalDate(2020, 2, 1), new LocalDate(2020, 3, 1), InvoiceItemType.RECURRING, new BigDecimal("100.00")));
+
+
+        // Cancel 2020-03-01
+        busHandler.pushExpectedEvents(NextEvent.CANCEL, NextEvent.BLOCK, NextEvent.NULL_INVOICE);
+        entitlement.cancelEntitlementWithPolicyOverrideBillingPolicy(EntitlementActionPolicy.IMMEDIATE, BillingActionPolicy.IMMEDIATE, ImmutableList.of(), callContext);
+        assertListenerStatus();
+
+        busHandler.pushExpectedEvents(NextEvent.NULL_INVOICE);
+        clock.addMonths(1);
+        assertListenerStatus();
+
+        for (int i= 0; i < 7; i++) {
+            clock.addMonths(1);
+            assertListenerStatus();
+        }
+
+    }
 
     @Test(groups = "slow")
     public void testApplyCreditOnExistingBalance() throws Exception {

beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestWithBCDUpdate.java
@@ -665,7 +665,7 @@ public class TestWithBCDUpdate extends TestIntegrationBase {
         // Change to the paying plan (alignment is CHANGE_OF_PLAN: we end up in TRIAL)
         // Extra NULL_INVOICE event because invoice computes a future notification effective right away
         final PlanPhaseSpecifier specWithTrial = new PlanPhaseSpecifier("Blowdart", BillingPeriod.MONTHLY, "trial", null);
-        busHandler.pushExpectedEvents(NextEvent.CHANGE, NextEvent.NULL_INVOICE, NextEvent.INVOICE);
+        busHandler.pushExpectedEvents(NextEvent.CHANGE,  NextEvent.INVOICE);
         baseEntitlement.changePlanOverrideBillingPolicy(new DefaultEntitlementSpecifier(specWithTrial), clock.getUTCToday(), BillingActionPolicy.IMMEDIATE, ImmutableList.<PluginProperty>of(), callContext);
         assertListenerStatus();
 

beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestWithInvoiceHardening.java
@@ -119,7 +119,7 @@ public class TestWithInvoiceHardening extends TestIntegrationBase {
         assertListenerStatus();
 
 
-        busHandler.pushExpectedEvents(NextEvent.BLOCK, NextEvent.NULL_INVOICE, NextEvent.INVOICE);
+        busHandler.pushExpectedEvents(NextEvent.BLOCK, NextEvent.INVOICE);
         final BlockingState blockingState5 = new DefaultBlockingState(entitlementId, BlockingStateType.SUBSCRIPTION, "UNBLOCK_4_27", "SERVICE_4_27", false, false, false, null);
         subscriptionApi.addBlockingState(blockingState5, new LocalDate(2019, 4, 27), ImmutableList.<PluginProperty>of(), callContext);
         assertListenerStatus();

beatrix/src/test/resources/catalogs/default/catalogTest.xml
@@ -255,6 +255,32 @@
                 </recurring>
             </finalPhase>
         </plan>
+        <plan name="blowdart-in-arrear-monthly-notrial">
+            <product>Blowdart</product>
+            <recurringBillingMode>IN_ARREAR</recurringBillingMode>
+            <finalPhase type="EVERGREEN">
+                <duration>
+                    <unit>UNLIMITED</unit>
+                </duration>
+                <recurring>
+                    <billingPeriod>MONTHLY</billingPeriod>
+                    <recurringPrice>
+                        <price>
+                            <currency>USD</currency>
+                            <value>100.00</value>
+                        </price>
+                        <price>
+                            <currency>EUR</currency>
+                            <value>100.00</value>
+                        </price>
+                        <price>
+                            <currency>GBP</currency>
+                            <value>100.00</value>
+                        </price>
+                    </recurringPrice>
+                </recurring>
+            </finalPhase>
+        </plan>
         <plan name="blowdart-quarterly-notrial">
             <product>Blowdart</product>
             <finalPhase type="EVERGREEN">
@@ -1552,6 +1578,11 @@
                 <plan>knife-monthly-notrial</plan>
                 <plan>blowdart-monthly-notrial</plan>
                 <plan>pistol-monthly-notrial</plan>
+            </plans>
+        </childPriceList>
+        <childPriceList name="inArrearNotrial">
+            <plans>
+                <plan>blowdart-in-arrear-monthly-notrial</plan>
             </plans>
         </childPriceList>
         <childPriceList name="trial">