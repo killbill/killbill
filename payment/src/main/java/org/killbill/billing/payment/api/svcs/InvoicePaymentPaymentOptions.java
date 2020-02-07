killbill/killbill

commit 3d411ecee93a8202513119b717d479d36d690ddc

payment: Fix ordering for control plugins for invoice payment to have first __INVOICE_PAYMENT_CONTROL_PLUGIN__. See #1276

payment/src/main/java/org/killbill/billing/payment/api/svcs/InvoicePaymentPaymentOptions.java
@@ -42,10 +42,13 @@ public class InvoicePaymentPaymentOptions implements PaymentOptions {
 
     public static List<String> addInvoicePaymentControlPlugin(final Collection<String> controlPluginNamesFromUser) {
         final List<String> paymentControlPluginNames = new LinkedList<String>();
-        paymentControlPluginNames.addAll(controlPluginNamesFromUser);
-        if (!paymentControlPluginNames.contains(InvoicePaymentControlPluginApi.PLUGIN_NAME)) {
-            paymentControlPluginNames.add(InvoicePaymentControlPluginApi.PLUGIN_NAME);
+        // Remove InvoicePaymentControlPluginApi.PLUGIN_NAME if already in the list
+        if (paymentControlPluginNames.contains(InvoicePaymentControlPluginApi.PLUGIN_NAME)) {
+            paymentControlPluginNames.remove(InvoicePaymentControlPluginApi.PLUGIN_NAME);
         }
+        // ... and add it FIRST
+        paymentControlPluginNames.add(InvoicePaymentControlPluginApi.PLUGIN_NAME);
+        paymentControlPluginNames.addAll(controlPluginNamesFromUser);
         return paymentControlPluginNames;
     }
 

payment/src/test/java/org/killbill/billing/payment/api/TestPaymentApi.java
@@ -48,10 +48,8 @@ import org.killbill.billing.payment.provider.DefaultNoOpPaymentMethodPlugin;
 import org.killbill.billing.payment.provider.ExternalPaymentProviderPlugin;
 import org.killbill.billing.payment.provider.MockPaymentControlProviderPlugin;
 import org.killbill.billing.payment.provider.MockPaymentProviderPlugin;
-import org.killbill.billing.payment.retry.PaymentRetryNotificationKey;
 import org.killbill.billing.util.entity.Pagination;
 import org.killbill.bus.api.PersistentBus.EventBusException;
-import org.killbill.notificationq.api.NotificationEventWithMetadata;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -867,17 +865,22 @@ public class TestPaymentApi extends PaymentTestSuiteWithEmbeddedDB {
                                                             requestedAmount,
                                                             new BigDecimal("1.0"),
                                                             Currency.USD));
+
         invoicePaymentApi.createPurchaseForInvoicePayment(account,
                                                           invoice.getId(),
                                                           account.getPaymentMethodId(),
                                                           null,
-                                                          requestedAmount,
+                                                          null,
                                                           Currency.USD,
                                                           null,
                                                           paymentExternalKey,
                                                           transactionExternalKey,
                                                           ImmutableList.<PluginProperty>of(),
-                                                          INVOICE_PAYMENT,
+                                                          /*
+                                                           * We explicitly don't pass InvoicePaymentControlPluginApi.PLUGIN_NAME
+                                                             to verify it it automatically added
+                                                          */
+                                                          CONTROL_PLUGIN_OPTIONS,
                                                           callContext);
         final Payment payment = paymentApi.getPaymentByExternalKey(paymentExternalKey, false, false, ImmutableList.<PluginProperty>of(), callContext);
         assertEquals(payment.getExternalKey(), paymentExternalKey);

commit c7b9a451632bf8d3507c2a59839a4d1960541a39

payment: Fix issue with control plugins insertion list introduced in 3d411ecee93. See #1276

payment/src/main/java/org/killbill/billing/payment/api/svcs/InvoicePaymentPaymentOptions.java
@@ -42,13 +42,15 @@ public class InvoicePaymentPaymentOptions implements PaymentOptions {
 
     public static List<String> addInvoicePaymentControlPlugin(final Collection<String> controlPluginNamesFromUser) {
         final List<String> paymentControlPluginNames = new LinkedList<String>();
-        // Remove InvoicePaymentControlPluginApi.PLUGIN_NAME if already in the list
-        if (paymentControlPluginNames.contains(InvoicePaymentControlPluginApi.PLUGIN_NAME)) {
-            paymentControlPluginNames.remove(InvoicePaymentControlPluginApi.PLUGIN_NAME);
-        }
-        // ... and add it FIRST
+        // Add InvoicePaymentControlPluginApi.PLUGIN_NAME FIRST
         paymentControlPluginNames.add(InvoicePaymentControlPluginApi.PLUGIN_NAME);
-        paymentControlPluginNames.addAll(controlPluginNamesFromUser);
+        for (String userPlugin : controlPluginNamesFromUser) {
+            if (userPlugin.equals(InvoicePaymentControlPluginApi.PLUGIN_NAME)) {
+                // skip in case it was part of the input list
+                continue;
+            }
+            paymentControlPluginNames.add(userPlugin);
+        }
         return paymentControlPluginNames;
     }
 