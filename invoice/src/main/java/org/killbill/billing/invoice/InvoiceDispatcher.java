killbill/killbill

commit cc14baff47e1f7ea6bf295cc7829b6dca4f53c70

invoice: Remove unsued method

invoice/src/main/java/org/killbill/billing/invoice/InvoiceDispatcher.java
@@ -887,20 +887,6 @@ public class InvoiceDispatcher {
     }
 
     public void setChargedThroughDates(final Invoice invoice, final InternalCallContext context) throws SubscriptionBaseApiException {
-        // Don't use invoice.getInvoiceItems(final Class<T> clazz) as some items can come from plugins
-        final Collection<InvoiceItem> invoiceItemsToConsider = new LinkedList<InvoiceItem>();
-        for (final InvoiceItem invoiceItem : invoice.getInvoiceItems()) {
-            switch (invoiceItem.getInvoiceItemType()) {
-                case FIXED:
-                case RECURRING:
-                case USAGE:
-                    invoiceItemsToConsider.add(invoiceItem);
-                    break;
-                default:
-                    break;
-            }
-        }
-
         final Map<UUID, DateTime> chargeThroughDates = InvoiceWithMetadata.computeChargedThroughDates(invoice,context);
         setChargedThroughDates(chargeThroughDates, context);
     }
@@ -922,25 +908,6 @@ public class InvoiceDispatcher {
         }
     }
 
-    // TODO Dups
-    private void addInvoiceItemsToChargeThroughDates(final Map<UUID, DateTime> chargeThroughDates,
-                                                     final Collection<InvoiceItem> items,
-                                                     final InternalTenantContext internalTenantContext) {
-
-        for (final InvoiceItem item : items) {
-            final UUID subscriptionId = item.getSubscriptionId();
-            final LocalDate endDate = (item.getEndDate() != null) ? item.getEndDate() : item.getStartDate();
-
-            final DateTime proposedChargedThroughDate = internalTenantContext.toUTCDateTime(endDate);
-            if (chargeThroughDates.containsKey(subscriptionId)) {
-                if (chargeThroughDates.get(subscriptionId).isBefore(proposedChargedThroughDate)) {
-                    chargeThroughDates.put(subscriptionId, proposedChargedThroughDate);
-                }
-            } else {
-                chargeThroughDates.put(subscriptionId, proposedChargedThroughDate);
-            }
-        }
-    }
 
     public static class FutureAccountNotifications {
 

commit 14c72122a50f978e5baccd6fc3179679434d8ff6

invoice: Fix issue introduced in cc14baff

invoice/src/main/java/org/killbill/billing/invoice/InvoiceDispatcher.java
@@ -597,13 +597,9 @@ public class InvoiceDispatcher {
                 final BusInternalEvent event = new DefaultNullInvoiceEvent(accountId, clock.getUTCToday(),
                                                                            internalCallContext.getAccountRecordId(), internalCallContext.getTenantRecordId(), internalCallContext.getUserToken());
 
-                try {
-                    setChargedThroughDates(invoiceWithMetadata.getChargeThroughDates(), internalCallContext);
-                } catch (final SubscriptionBaseApiException e) {
-                    log.error("Failed handling SubscriptionBase change.", e);
-                    return null;
-                }
-
+                // Although we have a null invoice, it could be as a result of removing $0 USAGE (config#isUsageZeroAmountDisabled)
+                // and so we may still need to set the CTD for such subscriptions.
+                setChargedThroughDatesNoExceptions(invoiceWithMetadata.getChargeThroughDates(), internalCallContext);
                 commitInvoiceAndSetFutureNotifications(account, futureAccountNotifications, internalCallContext);
                 postEvent(event);
             }
@@ -657,17 +653,8 @@ public class InvoiceDispatcher {
                 // Commit invoice on disk
                 final ExistingInvoiceMetadata existingInvoiceMetadata = new ExistingInvoiceMetadata(existingInvoices);
                 commitInvoiceAndSetFutureNotifications(account, invoiceModelDao, billingEvents, trackingIds, futureAccountNotifications, existingInvoiceMetadata, internalCallContext);
+                setChargedThroughDatesNoExceptions(invoiceWithMetadata.getChargeThroughDates(), internalCallContext);
                 success = true;
-
-                // See https://github.com/killbill/killbill/issues/1296
-                if (invoice.getStatus() == InvoiceStatus.COMMITTED) {
-                    try {
-                        setChargedThroughDates(invoice, internalCallContext);
-                    } catch (final SubscriptionBaseApiException e) {
-                        log.error("Failed handling SubscriptionBase change.", e);
-                        return null;
-                    }
-                }
             }
         } finally {
             // Make sure we always set future notifications in case of errors
@@ -886,17 +873,29 @@ public class InvoiceDispatcher {
         return internalCallContextFactory.createCallContext(context);
     }
 
-    public void setChargedThroughDates(final Invoice invoice, final InternalCallContext context) throws SubscriptionBaseApiException {
+    public void setChargedThroughDates(final Invoice invoice, final InternalCallContext context) throws InvoiceApiException {
         final Map<UUID, DateTime> chargeThroughDates = InvoiceWithMetadata.computeChargedThroughDates(invoice,context);
         setChargedThroughDates(chargeThroughDates, context);
     }
 
-    public void setChargedThroughDates(final Map<UUID, DateTime> chargeThroughDates, final InternalCallContext context) throws SubscriptionBaseApiException {
-        for (final Entry<UUID, DateTime> entry : chargeThroughDates.entrySet()) {
-            if (entry.getKey() != null) {
-                final DateTime chargeThroughDate = entry.getValue();
-                subscriptionApi.setChargedThroughDate(entry.getKey(), chargeThroughDate, context);
+    private void setChargedThroughDatesNoExceptions(final Map<UUID, DateTime> chargeThroughDates, final InternalCallContext context) {
+        try {
+            setChargedThroughDates(chargeThroughDates, context);
+        } catch (final InvoiceApiException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void setChargedThroughDates(final Map<UUID, DateTime> chargeThroughDates, final InternalCallContext context) throws InvoiceApiException {
+        try {
+            for (final Entry<UUID, DateTime> entry : chargeThroughDates.entrySet()) {
+                if (entry.getKey() != null) {
+                    final DateTime chargeThroughDate = entry.getValue();
+                    subscriptionApi.setChargedThroughDate(entry.getKey(), chargeThroughDate, context);
+                }
             }
+        } catch (final SubscriptionBaseApiException e) {
+            throw new InvoiceApiException(ErrorCode.UNEXPECTED_ERROR, "Failed to set chargedThroughDates", e);
         }
     }
 

invoice/src/main/java/org/killbill/billing/invoice/api/user/DefaultInvoiceUserApi.java
@@ -663,16 +663,10 @@ public class DefaultInvoiceUserApi implements InvoiceUserApi {
     @Override
     public void commitInvoice(final UUID invoiceId, final CallContext context) throws InvoiceApiException {
         final InternalCallContext internalCallContext = internalCallContextFactory.createInternalCallContext(invoiceId, ObjectType.INVOICE, context);
-
-        // See https://github.com/killbill/killbill/issues/1296
-        final Invoice invoice = getInvoice(invoiceId, context);
-        try {
-            dispatcher.setChargedThroughDates(invoice, internalCallContext);
-        } catch (final SubscriptionBaseApiException e) {
-            throw new InvoiceApiException(e);
-        }
-
+        // Update invoice status first prior we update CTD as we typically don't update CTD for non committed invoices.
         dao.changeInvoiceStatus(invoiceId, InvoiceStatus.COMMITTED, internalCallContext);
+        final Invoice invoice = getInvoice(invoiceId, context);
+        dispatcher.setChargedThroughDates(invoice, internalCallContext);
     }
 
     @Override

invoice/src/main/java/org/killbill/billing/invoice/generator/InvoiceWithMetadata.java
@@ -59,11 +59,11 @@ public class InvoiceWithMetadata {
                                final Map<UUID, SubscriptionFutureNotificationDates> perSubscriptionFutureNotificationDates,
                                final boolean filterZeroUsageItems,
                                final InternalCallContext context) {
+        this.filterZeroUsageItems = filterZeroUsageItems;
         this.invoice = originalInvoice;
+        this.chargedThroughDates = computeChargedThroughDates(originalInvoice, context);
         this.perSubscriptionFutureNotificationDates = perSubscriptionFutureNotificationDates;
         this.trackingIds = trackingIds;
-        this.chargedThroughDates = computeChargedThroughDates(originalInvoice, context);
-        this.filterZeroUsageItems = filterZeroUsageItems;
         build();
     }
 
@@ -89,6 +89,7 @@ public class InvoiceWithMetadata {
 
 
         if (invoice != null ) {
+            // Filter $0 USAGE items if specified by config
             if (filterZeroUsageItems) {
                 final Iterable<InvoiceItem> resultingItems = Iterables.filter(invoice.getInvoiceItems(), new Predicate<InvoiceItem>() {
                     @Override
@@ -103,7 +104,7 @@ public class InvoiceWithMetadata {
                 invoice.addInvoiceItems(filteredItems);
             }
 
-            // If not resulting items, result a null invoice
+            // If no resulting items -> result a null invoice
             // (but we already computed all the stuff we care about: chargedThroughDates, trackingIds,..
             if (Iterables.isEmpty(invoice.getInvoiceItems())) {
                 invoice = null;