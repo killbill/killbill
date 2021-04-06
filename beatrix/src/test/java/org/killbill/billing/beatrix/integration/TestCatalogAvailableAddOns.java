653f5c3e252ba30e4c3675f9d69aeb2c90e17898
stephane brossier <stephane@kill-bill.org>
2021-04-06 09:25:00 -0700

    beatrix: Fix test name and missing assertion

--- beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestCatalogAvailableAddOns.java
+++ beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestCatalogAvailableAddOns.java
@@ -34,6 +34,7 @@ import org.killbill.billing.entitlement.api.Entitlement;
 import org.killbill.billing.entitlement.api.EntitlementApiException;
 import org.killbill.billing.payment.api.PluginProperty;
 import org.killbill.billing.platform.api.KillbillConfigSource;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.google.common.collect.ImmutableList;
@@ -50,7 +51,7 @@ public class TestCatalogAvailableAddOns extends TestIntegrationBase {
     }
 
     @Test(groups = "slow")
-    public void testXXX() throws Exception {
+    public void testPlanWithNewAddOns() throws Exception {
         // Catalog v1 starts in 2021-03-31
         // Catalog v2 starts in 2021-04-30
         // -> Start on catalog V1
@@ -73,6 +74,7 @@ public class TestCatalogAvailableAddOns extends TestIntegrationBase {
         final PlanPhaseSpecifier addOnSpec = new PlanPhaseSpecifier("Laser-Scope", BillingPeriod.MONTHLY, PriceListSet.DEFAULT_PRICELIST_NAME, null);
         try {
             entitlementApi.addEntitlement(bpEntitlement.getBundleId(), new DefaultEntitlementSpecifier(addOnSpec), clock.getUTCToday(), clock.getUTCToday(), false, ImmutableList.<PluginProperty>of(), callContext);
+            Assert.fail("Should fail to create Laser-scope add-on as it is not available");
         } catch (final EntitlementApiException e) {
             assertEquals(e.getCode(), ErrorCode.CAT_NO_SUCH_PRODUCT.getCode());
         }
