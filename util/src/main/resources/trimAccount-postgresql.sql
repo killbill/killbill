c469c4781a958b4d1058948e17e09aa383e10ee4
Stéphane Brossier <sbrossier@packet.com>
2021-05-24 15:07:05 -0700

    util: Remove non core tables from clean|wipe store procs. See 3d0014e375b7

--- util/src/main/resources/trimAccount-postgresql.sql
+++ util/src/main/resources/trimAccount-postgresql.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE PROCEDURE trimAccount(p_account_key varchar(36)) LANGUAGE plpgsql
+CREATE OR REPLACE PROCEDURE trimAccount(p_account_id varchar(36)) LANGUAGE plpgsql
 AS $$
 DECLARE
     v_account_record_id bigint;
     v_tenant_record_id bigint;
 
 BEGIN
-    select record_id, tenant_record_id from accounts WHERE external_key = p_account_key into v_account_record_id, v_tenant_record_id;
+    select record_id, tenant_record_id from accounts WHERE id = p_account_id into v_account_record_id, v_tenant_record_id;
 
     DELETE FROM account_email_history WHERE account_record_id = v_account_record_id and tenant_record_id = v_tenant_record_id;
     DELETE FROM account_emails WHERE account_record_id = v_account_record_id and tenant_record_id = v_tenant_record_id;
--- util/src/main/resources/trimAccount.sql
+++ util/src/main/resources/trimAccount.sql
@@ -1,12 +1,12 @@
 drop procedure if exists trimAccount;
 DELIMITER //
-CREATE PROCEDURE trimAccount(p_account_key varchar(36))
+CREATE PROCEDURE trimAccount(p_account_id varchar(36))
 BEGIN
 
     DECLARE v_account_record_id bigint /*! unsigned */;
     DECLARE v_tenant_record_id bigint /*! unsigned */;
 
-    select record_id, tenant_record_id from accounts WHERE external_key = p_account_key into v_account_record_id, v_tenant_record_id;
+    select record_id, tenant_record_id from accounts WHERE id = p_account_id into v_account_record_id, v_tenant_record_id;
 
     DELETE FROM account_email_history WHERE account_record_id = v_account_record_id and tenant_record_id = v_tenant_record_id;
     DELETE FROM account_emails WHERE account_record_id = v_account_record_id and tenant_record_id = v_tenant_record_id;
