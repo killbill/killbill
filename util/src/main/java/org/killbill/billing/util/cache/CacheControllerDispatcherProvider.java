9fa48f06dc6f879832cea2d5e792c6c0472401b0
Pierre-Alexandre Meyer <pierre@mouraf.org>
2021-08-19 21:03:41 +0100

    util: introduce new option to disable select cache(s)
    One can now specify:
    
      org.killbill.cache.disabled=account-bcd
    
    to disable the account BCD cache for instance.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- util/src/main/java/org/killbill/billing/util/cache/CacheControllerDispatcherProvider.java
+++ util/src/main/java/org/killbill/billing/util/cache/CacheControllerDispatcherProvider.java
@@ -1,7 +1,8 @@
 /*
- * Copyright 2010-2013 Ning, Inc.
- * Copyright 2014-2017 Groupon, Inc
- * Copyright 2014-2017 The Billing Project, LLC
+ * Copyright 2010-2014 Ning, Inc.
+ * Copyright 2014-2020 Groupon, Inc
+ * Copyright 2020-2021 Equinix, Inc
+ * Copyright 2014-2021 The Billing Project, LLC
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
@@ -28,6 +29,7 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 
 import org.killbill.billing.util.cache.Cachable.CacheType;
+import org.killbill.billing.util.config.definition.CacheConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,29 +42,40 @@ public class CacheControllerDispatcherProvider implements Provider<CacheControll
 
     private final CacheManager cacheManager;
     private final Set<BaseCacheLoader> cacheLoaders;
+    private final CacheConfig cacheConfig;
 
     @Inject
     public CacheControllerDispatcherProvider(final CacheManager cacheManager,
-                                             final Set<BaseCacheLoader> cacheLoaders) {
+                                             final Set<BaseCacheLoader> cacheLoaders,
+                                             final CacheConfig cacheConfig) {
         this.cacheManager = cacheManager;
         this.cacheLoaders = cacheLoaders;
+        this.cacheConfig = cacheConfig;
     }
 
+    @SuppressWarnings({"rawtypes", "unchecked"})
     @Override
     public CacheControllerDispatcher get() {
         final Map<CacheType, CacheController<Object, Object>> cacheControllers = new LinkedHashMap<CacheType, CacheController<Object, Object>>();
         for (final BaseCacheLoader cacheLoader : cacheLoaders) {
             final CacheType cacheType = cacheLoader.getCacheType();
 
-            final Cache cache = cacheManager.getCache(cacheType.getCacheName(), cacheType.getKeyType(), cacheType.getValueType());
-            if (cache == null) {
-                logger.warn("Cache for cacheName='{}' not configured", cacheLoader.getCacheType().getCacheName());
-                continue;
-            }
-            Preconditions.checkState(!cache.isClosed(), "Cache '%s' should not be closed", cacheType.getCacheName());
+            final CacheController<Object, Object> cacheController;
+            if (cacheConfig.getDisabledCaches() != null && cacheConfig.getDisabledCaches().contains(cacheType.getCacheName())) {
+                logger.info("Disabling cache for cacheName='{}'", cacheLoader.getCacheType().getCacheName());
+                cacheController = new NoOpCacheController(cacheLoader);
+            } else {
+                final Cache cache = cacheManager.getCache(cacheType.getCacheName(), cacheType.getKeyType(), cacheType.getValueType());
+                if (cache == null) {
+                    logger.warn("Cache for cacheName='{}' not configured", cacheLoader.getCacheType().getCacheName());
+                    continue;
+                }
+                Preconditions.checkState(!cache.isClosed(), "Cache '%s' should not be closed", cacheType.getCacheName());
 
-            final CacheController<Object, Object> killBillCacheController = new KillBillCacheController<Object, Object>(cache, cacheLoader);
-            cacheControllers.put(cacheType, killBillCacheController);
+                cacheController = new KillBillCacheController<Object, Object>(cache, cacheLoader);
+            }
+
+            cacheControllers.put(cacheType, cacheController);
         }
 
         return new CacheControllerDispatcher(cacheControllers);
