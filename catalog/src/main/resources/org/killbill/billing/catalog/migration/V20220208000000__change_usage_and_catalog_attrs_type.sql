31f35271a1eab034e7456e7ef029f2afef78321d
xsalefter <xsalefter@gmail.com>
2022-02-14 16:16:30 +0700

    fix incorrect decimal size for rolled_up_usage.amount

--- catalog/src/main/resources/org/killbill/billing/catalog/migration/V20220208000000__change_usage_and_catalog_attrs_type.sql
+++ catalog/src/main/resources/org/killbill/billing/catalog/migration/V20220208000000__change_usage_and_catalog_attrs_type.sql
@@ -1,4 +1,4 @@
-alter table rolled_up_usage modify amount decimal(17, 9) not null;
+alter table rolled_up_usage modify amount decimal(18, 9) not null;
 
 alter table catalog_override_block_definition modify bsize decimal(18, 9) not null;
 alter table catalog_override_block_definition modify max decimal(18, 9) null;
