029c535c6863b1cdf0b64d73444e182e21ea3dc3
jcperal <jperal@methodmaker.co.nz>
2022-02-14 21:06:44 +0800

    1575: modified test cases to avoid issues existing cases and version for new client

--- profiles/killbill/src/test/java/org/killbill/billing/jaxrs/TestCatalog.java
+++ profiles/killbill/src/test/java/org/killbill/billing/jaxrs/TestCatalog.java
@@ -285,10 +285,9 @@ public class TestCatalog extends TestJaxrsBase {
 
     @Test(groups = "slow", description = "Missing recurringBillingMode in JSON return 1576")
     public void testGetCatalogJSONWithRecurringBillingMode() throws Exception {
-        uploadTenantCatalog("org/killbill/billing/server/SpyCarBasic.xml", false);
+        uploadTenantCatalog("org/killbill/billing/server/SpyCarBasic.v3.xml", false);
         List<Catalog> catalogsJson = catalogApi.getCatalogJson(null, null, requestOptions);
         Assert.assertEquals(catalogsJson.size(), 1);
-        System.out.println(catalogsJson.get(0).getProducts().get(0).getPlans().toString());
         Assert.assertEquals(catalogsJson.get(0).getProducts().get(0).getPlans().get(0).getRecurringBillingMode(), BillingMode.IN_ADVANCE);
         Assert.assertNotEquals(catalogsJson.get(0).getProducts().get(1).getPlans().get(0).getRecurringBillingMode(), BillingMode.IN_ARREAR);
         Assert.assertNotEquals(catalogsJson.get(0).getProducts().get(2).getPlans().get(0).getRecurringBillingMode(), null);
