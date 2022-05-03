killbill/killbill

commit 39154fc341fa638117f83fa48ed9ffc9898c18fb

invoice: Fix issue with invoice split
Make sure we call plugin completion calls (e.g `onSuccessCall`) for each generated invoice.

Enhance beatrix `TestWithInvoicePlugin#testInvoiceGrouping` to correctly verify this and also add the missing assertions on the generated invoices.

invoice/src/main/java/org/killbill/billing/invoice/InvoiceDispatcher.java
@@ -687,8 +687,7 @@ public class InvoiceDispatcher {
         final LocalDate actualTargetDate = invoice.getTargetDate();
         boolean success = false;
 
-        List<DefaultInvoice> splitInvoices;
-
+        List<DefaultInvoice> splitInvoices = Collections.emptyList();
         final List<Invoice> resultingInvoices = new ArrayList<>();
         try {
             // Generate missing credit (> 0 for generation and < 0 for use) prior we call the plugin(s)
@@ -754,20 +753,31 @@ public class InvoiceDispatcher {
             success = true;
 
         } finally {
+            // We call invoice control plugin completion calls for each resulting invoice.
             if (success) {
-                final DefaultInvoice refreshedInvoice = new DefaultInvoice(invoiceDao.getById(invoice.getId(), internalCallContext));
-                startNano = System.nanoTime();
-                invoicePluginDispatcher.onSuccessCall(actualTargetDate, refreshedInvoice, accountInvoices.getInvoices(), false, isRescheduled, callContext, pluginProperties, internalCallContext);
-                invoiceTimings.put(InvoiceTiming.PLUGINS_COMPLETION_CALL, System.nanoTime() - startNano);
-
-                resultingInvoices.add(refreshedInvoice);
+                splitInvoices.stream()
+                             .forEach(i -> {
+                                 try {
+                                     final InvoiceModelDao refreshedInv = invoiceDao.getById(i.getId(), internalCallContext);
+                                     final DefaultInvoice refreshedInvoice = new DefaultInvoice(refreshedInv);
+                                     long startNano1 = System.nanoTime();
+                                     invoicePluginDispatcher.onSuccessCall(actualTargetDate, refreshedInvoice, accountInvoices.getInvoices(), false, isRescheduled, callContext, pluginProperties, internalCallContext);
+                                     invoiceTimings.put(InvoiceTiming.PLUGINS_COMPLETION_CALL, System.nanoTime() - startNano1);
+                                     resultingInvoices.add(refreshedInvoice);
+                                 } catch (final InvoiceApiException e) {
+                                     throw new IllegalStateException(String.format("Failed to fetch invoice %s from disk", i.getId()));
+                                 }
+                             });
             } else {
-                // Make sure we always set future notifications in case of errors
-                setFutureNotifications(account, futureAccountNotifications, internalCallContext);
+                splitInvoices.stream()
+                             .forEach(i -> {
+                                 // Make sure we always set future notifications in case of errors
+                                 setFutureNotifications(account, futureAccountNotifications, internalCallContext);
+                                 long startNano2 = System.nanoTime();
+                                 invoicePluginDispatcher.onFailureCall(actualTargetDate, invoice, accountInvoices.getInvoices(), false, isRescheduled, callContext, pluginProperties, internalCallContext);
+                                 invoiceTimings.put(InvoiceTiming.PLUGINS_COMPLETION_CALL, System.nanoTime() - startNano2);
+                             });
 
-                startNano = System.nanoTime();
-                invoicePluginDispatcher.onFailureCall(actualTargetDate, invoice, accountInvoices.getInvoices(), false, isRescheduled, callContext, pluginProperties, internalCallContext);
-                invoiceTimings.put(InvoiceTiming.PLUGINS_COMPLETION_CALL, System.nanoTime() - startNano);
             }
         }
         return new InvoicesWithFutureNotifications(resultingInvoices, futureAccountNotifications);

commit cc2fe2abae95460b3d16669988dc96294b201c5d

invoice: Fix regression introduced in 39154fc3

invoice/src/main/java/org/killbill/billing/invoice/InvoiceDispatcher.java
@@ -769,10 +769,10 @@ public class InvoiceDispatcher {
                                  }
                              });
             } else {
+                // Make sure we always set the future notifications in case of errors
+                setFutureNotifications(account, futureAccountNotifications, internalCallContext);
                 splitInvoices.stream()
                              .forEach(i -> {
-                                 // Make sure we always set future notifications in case of errors
-                                 setFutureNotifications(account, futureAccountNotifications, internalCallContext);
                                  long startNano2 = System.nanoTime();
                                  invoicePluginDispatcher.onFailureCall(actualTargetDate, invoice, accountInvoices.getInvoices(), false, isRescheduled, callContext, pluginProperties, internalCallContext);
                                  invoiceTimings.put(InvoiceTiming.PLUGINS_COMPLETION_CALL, System.nanoTime() - startNano2);