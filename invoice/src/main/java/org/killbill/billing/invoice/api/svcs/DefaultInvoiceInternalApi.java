killbill/killbill

commit 9f56e9344837c806a6122c02af08c8f7ef326c7d

invoice: Introduce an InvoicePayment#status to replace InvoicePayment#sucess. See #1553
The `success` boolean value is not flexible enough to understand when we have recorded `PENDING` payments
and implement the logic that is required to avoid ending with negative invoice balance.

invoice/src/main/java/org/killbill/billing/invoice/api/svcs/DefaultInvoiceInternalApi.java
@@ -40,6 +40,7 @@ import org.killbill.billing.invoice.api.InvoiceApiException;
 import org.killbill.billing.invoice.api.InvoiceApiHelper;
 import org.killbill.billing.invoice.api.InvoiceInternalApi;
 import org.killbill.billing.invoice.api.InvoicePayment;
+import org.killbill.billing.invoice.api.InvoicePaymentStatus;
 import org.killbill.billing.invoice.api.InvoicePaymentType;
 import org.killbill.billing.invoice.api.InvoiceStatus;
 import org.killbill.billing.invoice.api.WithAccountLock;
@@ -99,13 +100,13 @@ public class DefaultInvoiceInternalApi implements InvoiceInternalApi {
 
     @Override
     public void recordPaymentAttemptInit(final UUID invoiceId, final BigDecimal amount, final Currency currency, final Currency processedCurrency, final UUID paymentId, final UUID paymentAttemptId, final String transactionExternalKey, final DateTime paymentDate, final InternalCallContext context) throws InvoiceApiException {
-        final InvoicePayment invoicePayment = new DefaultInvoicePayment(InvoicePaymentType.ATTEMPT, paymentId, invoiceId, paymentDate, amount, currency, processedCurrency, transactionExternalKey, false);
+        final InvoicePayment invoicePayment = new DefaultInvoicePayment(InvoicePaymentType.ATTEMPT, paymentId, invoiceId, paymentDate, amount, currency, processedCurrency, transactionExternalKey, InvoicePaymentStatus.INIT);
         dao.notifyOfPaymentInit(new InvoicePaymentModelDao(invoicePayment), paymentAttemptId, context);
     }
 
     @Override
     public void recordPaymentAttemptCompletion(final UUID invoiceId, final BigDecimal amount, final Currency currency, final Currency processedCurrency, final UUID paymentId, final UUID paymentAttemptId, final String transactionExternalKey, final DateTime paymentDate, final boolean success, final InternalCallContext context) throws InvoiceApiException {
-        final InvoicePayment invoicePayment = new DefaultInvoicePayment(InvoicePaymentType.ATTEMPT, paymentId, invoiceId, paymentDate, amount, currency, processedCurrency, transactionExternalKey, success);
+        final InvoicePayment invoicePayment = new DefaultInvoicePayment(InvoicePaymentType.ATTEMPT, paymentId, invoiceId, paymentDate, amount, currency, processedCurrency, transactionExternalKey, InvoicePaymentStatus.SUCCESS);
         dao.notifyOfPaymentCompletion(new InvoicePaymentModelDao(invoicePayment), paymentAttemptId, context);
     }
 
@@ -174,7 +175,7 @@ public class DefaultInvoiceInternalApi implements InvoiceInternalApi {
     InvoicePayment getInvoicePayment(final UUID paymentId, final InvoicePaymentType type, final InternalTenantContext context) throws InvoiceApiException {
         final List<InvoicePaymentModelDao> invoicePayments = dao.getInvoicePaymentsByPaymentId(paymentId, context);
         return invoicePayments.stream()
-                .filter(input -> input.getType() == type && input.getSuccess())
+                .filter(input -> input.getType() == type && input.getStatus() == InvoicePaymentStatus.SUCCESS)
                 .findFirst()
                 .map(DefaultInvoicePayment::new)
                 .orElse(null);

invoice/src/main/java/org/killbill/billing/invoice/dao/DefaultInvoiceDao.java
@@ -57,6 +57,7 @@ import org.killbill.billing.invoice.api.Invoice;
 import org.killbill.billing.invoice.api.InvoiceApiException;
 import org.killbill.billing.invoice.api.InvoiceItem;
 import org.killbill.billing.invoice.api.InvoiceItemType;
+import org.killbill.billing.invoice.api.InvoicePaymentStatus;
 import org.killbill.billing.invoice.api.InvoicePaymentType;
 import org.killbill.billing.invoice.api.InvoiceStatus;
 import org.killbill.billing.invoice.api.user.DefaultInvoiceAdjustmentEvent;
@@ -128,6 +129,8 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
     private final ParentInvoiceCommitmentPoster parentInvoiceCommitmentPoster;
     private final TagInternalApi tagInternalApi;
     private final AuditDao auditDao;
+
+
 
     @Inject
     public DefaultInvoiceDao(final TagInternalApi tagInternalApi,
@@ -672,7 +675,7 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
 
             final List<InvoicePaymentModelDao> paymentsForId = transactional.getByPaymentId(paymentId.toString(), context);
             final InvoicePaymentModelDao payment = paymentsForId.stream()
-                                                                .filter(input -> input.getType() == InvoicePaymentType.ATTEMPT && input.getSuccess())
+                                                                .filter(input -> input.getType() == InvoicePaymentType.ATTEMPT && input.getStatus() == InvoicePaymentStatus.SUCCESS)
                                                                 .findFirst()
                                                                 .orElseThrow(() -> new InvoiceApiException(ErrorCode.INVOICE_PAYMENT_BY_ATTEMPT_NOT_FOUND, paymentId));
 
@@ -702,14 +705,14 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
                                          "and does not match input paymentId=" + paymentId);
 
                 // The pending entry already exists, bail out (no need to send events, or compute cba logic)
-                if (!existingRefund.getSuccess() && !success) {
+                if (existingRefund.getStatus() != InvoicePaymentStatus.SUCCESS && !success) {
                     return existingRefund;
                 }
 
                 // At this point, we expect the request to be a transition PENDING -> SUCCESS (SUCCESS -> SUCCESS is also tolerated)
                 Preconditions.checkState(success, "Found successful refund for transactionExternalKey=" + transactionExternalKey + "and does not match pending input");
                 // We only update date and the status
-                existingRefund.setSuccess(true);
+                existingRefund.setStatus(InvoicePaymentStatus.SUCCESS);
                 existingRefund.setPaymentDate(context.getCreatedDate());
                 transactional.updateAttempt(existingRefund.getId().toString(),
                                             existingRefund.getPaymentId().toString(),
@@ -719,14 +722,14 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
                                             existingRefund.getProcessedCurrency(),
                                             existingRefund.getPaymentCookieId(),
                                             existingRefund.getLinkedInvoicePaymentId().toString(),
-                                            existingRefund.getSuccess(),
+                                            existingRefund.getStatus().toString(),
                                             context);
                 result = existingRefund;
             } else {
                 final InvoicePaymentModelDao refund = new InvoicePaymentModelDao(UUIDs.randomUUID(), context.getCreatedDate(), InvoicePaymentType.REFUND,
                                                                                  payment.getInvoiceId(), paymentId,
                                                                                  context.getCreatedDate(), requestedPositiveAmount.negate(),
-                                                                                 payment.getCurrency(), payment.getProcessedCurrency(), transactionExternalKey, payment.getId(), success);
+                                                                                 payment.getCurrency(), payment.getProcessedCurrency(), transactionExternalKey, payment.getId(), InvoicePaymentStatus.SUCCESS);
                 result = createAndRefresh(transactional, refund, context);
             }
 
@@ -801,7 +804,7 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
             final InvoicePaymentModelDao chargeBack = new InvoicePaymentModelDao(UUIDs.randomUUID(), context.getCreatedDate(), InvoicePaymentType.CHARGED_BACK,
                                                                                  payment.getInvoiceId(), payment.getPaymentId(), context.getCreatedDate(),
                                                                                  requestedChargedBackAmount.negate(), payment.getCurrency(), payment.getProcessedCurrency(),
-                                                                                 chargebackTransactionExternalKey, payment.getId(), true);
+                                                                                 chargebackTransactionExternalKey, payment.getId(), InvoicePaymentStatus.SUCCESS);
             createAndRefresh(transactional, chargeBack, context);
 
             // Notify the bus since the balance of the invoice changed
@@ -836,7 +839,7 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
                                         invoicePayment.getProcessedCurrency(),
                                         invoicePayment.getPaymentCookieId(),
                                         invoicePayment.getLinkedInvoicePaymentId() == null ? null : invoicePayment.getLinkedInvoicePaymentId().toString(),
-                                        false,
+                                        InvoicePaymentStatus.INIT.toString(),
                                         context);
             final InvoicePaymentModelDao chargebackReversed = transactional.getByRecordId(invoicePayment.getRecordId(), context);
 
@@ -936,7 +939,7 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
         transactionalSqlDao.execute(false, entitySqlDaoWrapperFactory -> {
             final InvoicePaymentSqlDao transactional = entitySqlDaoWrapperFactory.become(InvoicePaymentSqlDao.class);
             //
-            // In case of notifyOfPaymentInit we always want to record the row with success = false
+            // In case of notifyOfPaymentInit we always want to record the row with status = INIT
             // Otherwise, if the payment id is null, the payment wasn't attempted (e.g. no payment method so we don't record an attempt but send
             // an event nonetheless (e.g. for Overdue))
             //
@@ -974,7 +977,7 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
                                                 invoicePayment.getProcessedCurrency(),
                                                 invoicePayment.getPaymentCookieId(),
                                                 null,
-                                                invoicePayment.getSuccess(),
+                                                invoicePayment.getStatus().toString(),
                                                 context);
                 }
             }
@@ -1142,7 +1145,7 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
                                            final UUID accountId, final UUID paymentAttemptId, final UUID userToken,
                                            final InternalCallContext context) {
         final BusEvent busEvent;
-        if (Boolean.TRUE.equals(invoicePaymentModelDao.getSuccess())) {
+        if (InvoicePaymentStatus.SUCCESS == invoicePaymentModelDao.getStatus()) {
             busEvent = new DefaultInvoicePaymentInfoEvent(accountId,
                                                           invoicePaymentModelDao.getPaymentId(),
                                                           paymentAttemptId,

commit 58f8cb36592ece9603240e4b30107a948b66f0c9

invoice, beatrix: Fix issues introduced in 9f56e93448. See #1533

invoice/src/main/java/org/killbill/billing/invoice/api/svcs/DefaultInvoiceInternalApi.java
@@ -106,7 +106,8 @@ public class DefaultInvoiceInternalApi implements InvoiceInternalApi {
 
     @Override
     public void recordPaymentAttemptCompletion(final UUID invoiceId, final BigDecimal amount, final Currency currency, final Currency processedCurrency, final UUID paymentId, final UUID paymentAttemptId, final String transactionExternalKey, final DateTime paymentDate, final boolean success, final InternalCallContext context) throws InvoiceApiException {
-        final InvoicePayment invoicePayment = new DefaultInvoicePayment(InvoicePaymentType.ATTEMPT, paymentId, invoiceId, paymentDate, amount, currency, processedCurrency, transactionExternalKey, InvoicePaymentStatus.SUCCESS);
+        final InvoicePaymentStatus status = success ? InvoicePaymentStatus.SUCCESS : InvoicePaymentStatus.INIT;
+        final InvoicePayment invoicePayment = new DefaultInvoicePayment(InvoicePaymentType.ATTEMPT, paymentId, invoiceId, paymentDate, amount, currency, processedCurrency, transactionExternalKey, status);
         dao.notifyOfPaymentCompletion(new InvoicePaymentModelDao(invoicePayment), paymentAttemptId, context);
     }
 

invoice/src/main/java/org/killbill/billing/invoice/dao/DefaultInvoiceDao.java
@@ -726,10 +726,11 @@ public class DefaultInvoiceDao extends EntityDaoBase<InvoiceModelDao, Invoice, I
                                             context);
                 result = existingRefund;
             } else {
+                final InvoicePaymentStatus status = success ? InvoicePaymentStatus.SUCCESS : InvoicePaymentStatus.INIT;
                 final InvoicePaymentModelDao refund = new InvoicePaymentModelDao(UUIDs.randomUUID(), context.getCreatedDate(), InvoicePaymentType.REFUND,
                                                                                  payment.getInvoiceId(), paymentId,
                                                                                  context.getCreatedDate(), requestedPositiveAmount.negate(),
-                                                                                 payment.getCurrency(), payment.getProcessedCurrency(), transactionExternalKey, payment.getId(), InvoicePaymentStatus.SUCCESS);
+                                                                                 payment.getCurrency(), payment.getProcessedCurrency(), transactionExternalKey, payment.getId(), status);
                 result = createAndRefresh(transactional, refund, context);
             }
 

beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestInvoicePayment.java
@@ -754,7 +754,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -823,7 +823,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -861,7 +861,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice11.getPayments().size(), 1);
         assertEquals(invoice11.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice11.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice11.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice11.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice11.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance11 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -978,7 +978,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice2.getPayments().size(), 1);
         assertEquals(invoice2.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice2.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice2.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice2.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertEquals(invoice2.getPayments().get(0).getPaymentId(), payment1.getId());
 
         final BigDecimal accountBalance2 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1057,7 +1057,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(new BigDecimal("249.95")), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1173,7 +1173,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(new BigDecimal("249.95")), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1302,7 +1302,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice2.getPayments().size(), 1);
         assertEquals(invoice2.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice2.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice2.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice2.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice2.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance2 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1378,7 +1378,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1479,7 +1479,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         dbi.withHandle(new HandleCallback<Void>() {
             @Override
             public Void withHandle(final Handle handle) throws Exception {
-                handle.execute("update invoice_payments set success = false where payment_cookie_id = ?", originalTransaction.getExternalKey());
+                handle.execute("update invoice_payments set status='INIT' where payment_cookie_id = ?", originalTransaction.getExternalKey());
                 return null;
             }
         });
@@ -1578,7 +1578,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1715,7 +1715,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1834,7 +1834,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);
@@ -1943,7 +1943,7 @@ public class TestInvoicePayment extends TestIntegrationBase {
         assertEquals(invoice1.getPayments().size(), 1);
         assertEquals(invoice1.getPayments().get(0).getAmount().compareTo(BigDecimal.ZERO), 0);
         assertEquals(invoice1.getPayments().get(0).getCurrency(), Currency.USD);
-        assertFalse(invoice1.getPayments().get(0).getStatus() != InvoicePaymentStatus.SUCCESS);
+        assertFalse(invoice1.getPayments().get(0).getStatus() == InvoicePaymentStatus.SUCCESS);
         assertNotNull(invoice1.getPayments().get(0).getPaymentId());
 
         final BigDecimal accountBalance1 = invoiceUserApi.getAccountBalance(account.getId(), callContext);