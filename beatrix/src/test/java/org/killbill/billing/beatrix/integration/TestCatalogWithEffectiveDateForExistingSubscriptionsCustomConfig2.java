3aac5d1f2696c5371994f2318e5434d4efcf4b35
stephane brossier <stephane@kill-bill.org>
2023-04-17 13:41:34 -0700

    beatrix: Small test cleanup

--- beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestCatalogWithEffectiveDateForExistingSubscriptionsCustomConfig2.java
+++ beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestCatalogWithEffectiveDateForExistingSubscriptionsCustomConfig2.java
@@ -18,7 +18,6 @@
 package org.killbill.billing.beatrix.integration;
 
 import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,11 +26,9 @@ import org.killbill.billing.account.api.Account;
 import org.killbill.billing.api.TestApiListener.NextEvent;
 import org.killbill.billing.beatrix.util.InvoiceChecker.ExpectedInvoiceItemCheck;
 import org.killbill.billing.catalog.api.BillingPeriod;
-import org.killbill.billing.catalog.api.PlanPhaseSpecifier;
 import org.killbill.billing.catalog.api.ProductCategory;
 import org.killbill.billing.catalog.api.VersionedCatalog;
 import org.killbill.billing.entitlement.api.DefaultEntitlement;
-import org.killbill.billing.entitlement.api.DefaultEntitlementSpecifier;
 import org.killbill.billing.invoice.api.Invoice;
 import org.killbill.billing.invoice.api.InvoiceItemType;
 import org.killbill.billing.platform.api.KillbillConfigSource;
@@ -51,18 +48,23 @@ public class TestCatalogWithEffectiveDateForExistingSubscriptionsCustomConfig2 e
         return super.getConfigSource(null, allExtraProperties);
     }
 
-    @Test(groups = "slow", description="https://github.com/killbill/killbill/issues/1842")
-    public void testSubscriptionWithBothAccounts() throws Exception {  //test for the complete scenario described in #1842
-    	
+    @Test(groups = "slow", description = "https://github.com/killbill/killbill/issues/1842")
+    public void testSubscriptionWithBothAccounts() throws Exception {
+
         final LocalDate today = new LocalDate(2023, 3, 28);
         clock.setDay(today);
 
         final VersionedCatalog catalog = catalogUserApi.getCatalog("foo", callContext);
 
+        //
+        // We have 2 catalog versions, V2 is effective on 2023-04-07 but there is an effectiveDateForExistingSubscriptions=2023-04-20 for the Liability Plan.
+        // - Account1 has a BCD set after the 20, so it worked as expected
+        // - Account2 has a BCD set before the 20, and it did not work as expected as per #1842
+        //
         final boolean isAccount1 = true;
         final boolean isAccount2 = true;
 
-        final Account account1 = createAccountWithNonOsgiPaymentMethod(getAccountData(28)); //TODO remove bcd?
+        final Account account1 = createAccountWithNonOsgiPaymentMethod(getAccountData(28));
         Invoice curInvoice1;
         if (isAccount1) {
             final DefaultEntitlement bpEntitlement1 =
@@ -72,14 +74,13 @@ public class TestCatalogWithEffectiveDateForExistingSubscriptionsCustomConfig2 e
 
             assertNotNull(bpEntitlement1);
             curInvoice1 = invoiceChecker.checkInvoice(account1.getId(), 1, callContext,
-                                                              new ExpectedInvoiceItemCheck(new LocalDate(2023, 3, 28), new LocalDate(2023, 4, 28), InvoiceItemType.RECURRING, new BigDecimal("49.95")));
+                                                      new ExpectedInvoiceItemCheck(new LocalDate(2023, 3, 28), new LocalDate(2023, 4, 28), InvoiceItemType.RECURRING, new BigDecimal("49.95")));
             Assert.assertEquals(curInvoice1.getInvoiceItems().get(0).getCatalogEffectiveDate().toDate().compareTo(catalog.getVersions().get(0).getEffectiveDate()), 0);
         }
 
-
         clock.setDay(new LocalDate(2023, 4, 6));
-        
-        final Account account2 = createAccountWithNonOsgiPaymentMethod(getAccountData(6)); //TODO remove bcd?
+
+        final Account account2 = createAccountWithNonOsgiPaymentMethod(getAccountData(6));
         Invoice curInvoice2;
         if (isAccount2) {
             final DefaultEntitlement bpEntitlement2 =
@@ -89,7 +90,7 @@ public class TestCatalogWithEffectiveDateForExistingSubscriptionsCustomConfig2 e
 
             assertNotNull(bpEntitlement2);
             curInvoice2 = invoiceChecker.checkInvoice(account2.getId(), 1, callContext,
-                                                              new ExpectedInvoiceItemCheck(new LocalDate(2023, 4, 6), new LocalDate(2023, 5, 6), InvoiceItemType.RECURRING, new BigDecimal("49.95")));
+                                                      new ExpectedInvoiceItemCheck(new LocalDate(2023, 4, 6), new LocalDate(2023, 5, 6), InvoiceItemType.RECURRING, new BigDecimal("49.95")));
             Assert.assertEquals(curInvoice2.getInvoiceItems().get(0).getCatalogEffectiveDate().toDate().compareTo(catalog.getVersions().get(0).getEffectiveDate()), 0);
         }
 
@@ -103,17 +104,15 @@ public class TestCatalogWithEffectiveDateForExistingSubscriptionsCustomConfig2 e
         assertListenerStatus();
 
         if (isAccount1) {
-            curInvoice1 = invoiceChecker.checkInvoice(account1.getId(), 2, callContext,
-                                                      new ExpectedInvoiceItemCheck(new LocalDate(2023, 4, 28), new LocalDate(2023, 5, 28), InvoiceItemType.RECURRING, new BigDecimal("59.95")));
+            invoiceChecker.checkInvoice(account1.getId(), 2, callContext,
+                                        new ExpectedInvoiceItemCheck(new LocalDate(2023, 4, 28), new LocalDate(2023, 5, 28), InvoiceItemType.RECURRING, new BigDecimal("59.95")));
         }
 
         if (isAccount2) {
-            curInvoice2 = invoiceChecker.checkInvoice(account2.getId(), 2, callContext,
-                                                      new ExpectedInvoiceItemCheck(new LocalDate(2023, 5, 6), new LocalDate(2023, 6, 6), InvoiceItemType.RECURRING, new BigDecimal("59.95"))); //FAILS here, invoice created for 49.95 as per v1
+            invoiceChecker.checkInvoice(account2.getId(), 2, callContext,
+                                        new ExpectedInvoiceItemCheck(new LocalDate(2023, 5, 6), new LocalDate(2023, 6, 6), InvoiceItemType.RECURRING, new BigDecimal("59.95"))); //FAILS here, invoice created for 49.95 as per v1
         }
-
         assertListenerStatus();
     }
-
 
 }
