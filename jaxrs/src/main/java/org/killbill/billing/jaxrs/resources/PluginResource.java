eb8ae48fc977db40f7258f032bac029470304f18
KBbitsP <75751774+KBbitsP@users.noreply.github.com>
2024-04-12 23:15:24 +0530

    https://github.com/killbill/killbill/issues/1975

--- jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/PluginResource.java
+++ jaxrs/src/main/java/org/killbill/billing/jaxrs/resources/PluginResource.java
@@ -256,8 +256,7 @@ public class PluginResource extends JaxRsResourceBase {
             data.put(key, form.getFirst(key));
         }
         appendFormParametersToBody(out, data);
-
-        request.getInputStream().transferTo(out);
+    // changes related https://github.com/killbill/killbill/issues/1975 : Removing request.getInputStream().transferTo(out);
         return new ByteArrayInputStream(out.toByteArray());
     }
 
