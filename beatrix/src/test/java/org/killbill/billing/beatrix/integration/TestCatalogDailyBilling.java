acc37af94514b795f12bcca52a7c112bacfd434d
reshmabidikar <reshmabidikar@outlook.com>
2024-04-19 09:30:15 +0530

    Corrections in tests

--- beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestCatalogDailyBilling.java
+++ beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestCatalogDailyBilling.java
@@ -27,19 +27,17 @@ import java.util.UUID;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
+import org.killbill.billing.ErrorCode;
 import org.killbill.billing.account.api.Account;
 import org.killbill.billing.api.TestApiListener.NextEvent;
 import org.killbill.billing.beatrix.util.InvoiceChecker.ExpectedInvoiceItemCheck;
 import org.killbill.billing.catalog.api.PlanPhaseSpecifier;
 import org.killbill.billing.entitlement.api.DefaultEntitlementSpecifier;
-import org.killbill.billing.entitlement.api.Entitlement;
 import org.killbill.billing.invoice.api.Invoice;
+import org.killbill.billing.invoice.api.InvoiceApiException;
 import org.killbill.billing.invoice.api.InvoiceItemType;
-import org.killbill.billing.payment.api.PluginProperty;
 import org.killbill.billing.platform.api.KillbillConfigSource;
 import org.testng.annotations.Test;
-
-import com.google.common.collect.ImmutableList;
 
 import static org.testng.Assert.assertEquals;
 
@@ -55,76 +53,88 @@ public class TestCatalogDailyBilling extends TestIntegrationBase  {
 
     @Test(description="https://github.com/killbill/killbill/issues/2005")
     public void testDailyBillingNYTimezone() throws Exception {
-        final LocalDate initialDate = new LocalDate(2024, 4, 12);
-        clock.setDay(initialDate);
+        final DateTime initialDateTime = new DateTime("2024-04-12T08:22:28");
+        clock.setTime(initialDateTime);
 
         final DateTimeZone timezone = DateTimeZone.forID("America/New_York");
-        final DateTime referenceTime = new DateTime("2024-04-12T08:22:28");
-        final Account account = createAccountWithNonOsgiPaymentMethod(getAccountData(0, timezone, referenceTime));
+        final Account account = createAccountWithNonOsgiPaymentMethod(getAccountData(0, timezone, initialDateTime));
 
         final PlanPhaseSpecifier spec = new PlanPhaseSpecifier("plan-b");
         busHandler.pushExpectedEvents(NextEvent.CREATE, NextEvent.BLOCK, NextEvent.INVOICE);
         final UUID createdEntitlementId = entitlementApi.createBaseEntitlement(account.getId(), new DefaultEntitlementSpecifier(spec), account.getExternalKey(), null, null, false, true, Collections.emptyList(), callContext);
         assertListenerStatus();
 
-        //only 1 invoice as expected
+        // Invoice corresponding to TRIAL phase
         List<Invoice> invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
         assertEquals(invoices.size(), 1);
 
-        //start date in invoice is 2024-04-11 - Is this expected?
-        Invoice curInvoice = invoiceChecker.checkInvoice(account.getId(), 1, callContext,
-                                                         new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 11), null, InvoiceItemType.FIXED, BigDecimal.ZERO));
-
-        // test fails here as two invoices are created
-        busHandler.pushExpectedEvents(NextEvent.PHASE, NextEvent.INVOICE, NextEvent.PAYMENT, NextEvent.INVOICE_PAYMENT, NextEvent.INVOICE, NextEvent.PAYMENT, NextEvent.INVOICE_PAYMENT);
-        clock.setTime(new DateTime("2024-04-13T08:22:28"));
-        assertListenerStatus();
-
-        //Total 3 invoices
-        invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
-        assertEquals(invoices.size(), 3);
-
-
-        //Invoice with startDate=2024-04-12 and endDate=2024-04-13
-        curInvoice = invoiceChecker.checkInvoice(account.getId(), 2, callContext,
-                                                         new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 12), new LocalDate(2024, 4, 13), InvoiceItemType.RECURRING, BigDecimal.ONE));
-
-        //Invoice with startDate=2024-04-13 and endDate=2024-04-14
-        curInvoice = invoiceChecker.checkInvoice(account.getId(), 3, callContext,
-                                                 new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 13), new LocalDate(2024, 4, 14), InvoiceItemType.RECURRING, BigDecimal.ONE));
-
-
-
-    }
-
-    @Test(description="https://github.com/killbill/killbill/issues/2005")
-    public void testDailyBillingUTCTimezone() throws Exception {
-        final LocalDate initialDate = new LocalDate(2024, 4, 12);
-        clock.setDay(initialDate);
-
-        final DateTimeZone timezone = DateTimeZone.UTC;
-        final DateTime referenceTime = new DateTime("2024-04-12T08:22:28");
-        final Account account = createAccountWithNonOsgiPaymentMethod(getAccountData(0, timezone, referenceTime));
-
-        final PlanPhaseSpecifier spec = new PlanPhaseSpecifier("plan-b");
-        busHandler.pushExpectedEvents(NextEvent.CREATE, NextEvent.BLOCK, NextEvent.INVOICE);
-        final UUID createdEntitlementId = entitlementApi.createBaseEntitlement(account.getId(), new DefaultEntitlementSpecifier(spec), account.getExternalKey(), null, null, false, true, Collections.emptyList(), callContext);
-        assertListenerStatus();
-
-        //only 1 invoice as expected
-        List<Invoice> invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
-        assertEquals(invoices.size(), 1);
-
-        //start date in invoice is 2024-04-12
         Invoice curInvoice = invoiceChecker.checkInvoice(account.getId(), 1, callContext,
                                                          new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 12), null, InvoiceItemType.FIXED, BigDecimal.ZERO));
 
-        // Works as expected here - only 1 invoice
+        // Move clock to 4/13 - Invoice generated
         busHandler.pushExpectedEvents(NextEvent.PHASE, NextEvent.INVOICE, NextEvent.PAYMENT, NextEvent.INVOICE_PAYMENT);
         clock.setTime(new DateTime("2024-04-13T08:22:28"));
         assertListenerStatus();
 
-        //Total 2 invoices
+        // Total 2 invoices
+        invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
+        assertEquals(invoices.size(), 2);
+
+        //Invoice with startDate=2024-04-13 and endDate=2024-04-14
+        curInvoice = invoiceChecker.checkInvoice(account.getId(), 2, callContext,
+                                                 new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 13), new LocalDate(2024, 4, 14), InvoiceItemType.RECURRING, BigDecimal.ONE));
+
+        //Trigger invoice run with target date = 2024-04-13 - No invoice generated
+        busHandler.pushExpectedEvents(NextEvent.NULL_INVOICE);
+        try {
+            final LocalDate targetDate = new LocalDate("2024-04-13");
+            invoiceUserApi.triggerInvoiceGeneration(account.getId(), targetDate, Collections.emptyList(), callContext);
+        }
+        catch(InvoiceApiException e) {
+            assertEquals(e.getCode(), ErrorCode.INVOICE_NOTHING_TO_DO.getCode());
+        }
+        assertListenerStatus();
+
+        // Move clock to 4/14 - Invoice generated
+        busHandler.pushExpectedEvents(NextEvent.INVOICE, NextEvent.PAYMENT, NextEvent.INVOICE_PAYMENT);
+        clock.setTime(new DateTime("2024-04-14T08:22:28"));
+        assertListenerStatus();
+
+        // Total 3 invoices
+        invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
+        assertEquals(invoices.size(), 3);
+
+        //Invoice with startDate=2024-04-14 and endDate=2024-04-15
+        curInvoice = invoiceChecker.checkInvoice(account.getId(), 3, callContext,
+                                                 new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 14), new LocalDate(2024, 4, 15), InvoiceItemType.RECURRING, BigDecimal.ONE));
+    }
+
+    @Test(description="https://github.com/killbill/killbill/issues/2005")
+    public void testDailyBillingUTCTimezone() throws Exception {
+        final DateTime initialDateTime = new DateTime("2024-04-12T08:22:28");
+        clock.setTime(initialDateTime);
+
+        final DateTimeZone timezone = DateTimeZone.UTC;
+        final Account account = createAccountWithNonOsgiPaymentMethod(getAccountData(0, timezone, initialDateTime));
+
+        final PlanPhaseSpecifier spec = new PlanPhaseSpecifier("plan-b");
+        busHandler.pushExpectedEvents(NextEvent.CREATE, NextEvent.BLOCK, NextEvent.INVOICE);
+        final UUID createdEntitlementId = entitlementApi.createBaseEntitlement(account.getId(), new DefaultEntitlementSpecifier(spec), account.getExternalKey(), null, null, false, true, Collections.emptyList(), callContext);
+        assertListenerStatus();
+
+        // Invoice corresponding to TRIAL phase
+        List<Invoice> invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
+        assertEquals(invoices.size(), 1);
+
+        Invoice curInvoice = invoiceChecker.checkInvoice(account.getId(), 1, callContext,
+                                                         new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 12), null, InvoiceItemType.FIXED, BigDecimal.ZERO));
+
+        // Move clock to 4/13 - Invoice generated
+        busHandler.pushExpectedEvents(NextEvent.PHASE, NextEvent.INVOICE, NextEvent.PAYMENT, NextEvent.INVOICE_PAYMENT);
+        clock.setTime(new DateTime("2024-04-13T08:22:28"));
+        assertListenerStatus();
+
+        // Total 2 invoices
         invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
         assertEquals(invoices.size(), 2);
 
@@ -132,6 +142,31 @@ public class TestCatalogDailyBilling extends TestIntegrationBase  {
         //Invoice with startDate=2024-04-13 and endDate=2024-04-14
         curInvoice = invoiceChecker.checkInvoice(account.getId(), 2, callContext,
                                                  new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 13), new LocalDate(2024, 4, 14), InvoiceItemType.RECURRING, BigDecimal.ONE));
+
+        //Trigger invoice run with target date = 2024-04-13 - No invoice generated
+        busHandler.pushExpectedEvents(NextEvent.NULL_INVOICE);
+        try {
+            final LocalDate targetDate = new LocalDate("2024-04-13");
+            invoiceUserApi.triggerInvoiceGeneration(account.getId(), targetDate, Collections.emptyList(), callContext);
+        }
+        catch(InvoiceApiException e) {
+            assertEquals(e.getCode(), ErrorCode.INVOICE_NOTHING_TO_DO.getCode());
+        }
+        assertListenerStatus();
+
+        // Move clock to 4/14 - Invoice generated
+        busHandler.pushExpectedEvents(NextEvent.INVOICE, NextEvent.PAYMENT, NextEvent.INVOICE_PAYMENT);
+        clock.setTime(new DateTime("2024-04-14T08:22:28"));
+        assertListenerStatus();
+
+        // Total 3 invoices
+        invoices = invoiceUserApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
+        assertEquals(invoices.size(), 3);
+
+
+        //Invoice with startDate=2024-04-14 and endDate=2024-04-15
+        curInvoice = invoiceChecker.checkInvoice(account.getId(), 3, callContext,
+                                                 new ExpectedInvoiceItemCheck(new LocalDate(2024, 4, 14), new LocalDate(2024, 4, 15), InvoiceItemType.RECURRING, BigDecimal.ONE));
     }
 
 }
