ac754c7dc2766dcf4bd5dfda49a9fc2db4cc014f
reshmabidikar <reshmabidikar@outlook.com>
2024-07-10 11:27:02 +0530

    Fix for #2000

--- beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestIntegrationWithWrittenOffTag.java
+++ beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestIntegrationWithWrittenOffTag.java
@@ -17,6 +17,7 @@
 
 package org.killbill.billing.beatrix.integration;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +25,7 @@ import java.util.UUID;
 import javax.inject.Inject;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.killbill.billing.ObjectType;
 import org.killbill.billing.account.api.Account;
 import org.killbill.billing.api.TestApiListener.NextEvent;
@@ -32,6 +34,7 @@ import org.killbill.billing.catalog.api.ProductCategory;
 import org.killbill.billing.entitlement.api.DefaultEntitlement;
 import org.killbill.billing.invoice.api.Invoice;
 import org.killbill.billing.invoice.api.InvoiceUserApi;
+import org.killbill.billing.invoice.model.ExternalChargeInvoiceItem;
 import org.killbill.billing.payment.api.Payment;
 import org.killbill.billing.util.api.TagApiException;
 import org.killbill.billing.util.api.TagDefinitionApiException;
@@ -100,6 +103,35 @@ public class TestIntegrationWithWrittenOffTag extends TestIntegrationBase {
         List<Payment> accountPayments = paymentApi.getAccountPayments(account.getId(), false, false, Collections.emptyList(), callContext);
         assertEquals(accountPayments.size(), 0);
 
+    }
+
+    @Test(groups = "slow", description="https://github.com/killbill/killbill/issues/2000")
+    public void testRetrieveWrittenOffInvoice() throws Exception {
+        clock.setDay(new LocalDate(2024, 5, 1));
+        add_AUTO_PAY_OFF_Tag(account.getId(), ObjectType.ACCOUNT);
+
+        busHandler.pushExpectedEvents(NextEvent.INVOICE);
+        ExternalChargeInvoiceItem item = new ExternalChargeInvoiceItem(null, account.getId(), null, "", new LocalDate(2024, 5, 1), null, BigDecimal.TEN, account.getCurrency(), null);
+        invoiceUserApi.insertExternalCharges(account.getId(), null, List.of(item), true, null, callContext);
+        assertListenerStatus();
+
+        final List<Invoice> invoices = invoiceApi.getInvoicesByAccount(account.getId(), false, false, true, callContext);
+
+        //Verify balance=10 before setting the WRITTEN_OFF tag
+        Invoice invoice = invoiceApi.getInvoice(invoices.get(0).getId(), callContext);
+        assertEquals(invoice.getBalance().stripTrailingZeros().compareTo(BigDecimal.TEN), 0);
+
+        invoice = invoiceApi.getInvoiceByNumber(invoice.getInvoiceNumber(), callContext);
+        assertEquals(invoice.getBalance().stripTrailingZeros().compareTo(BigDecimal.TEN), 0);
+
+        add_WRITTEN_OFF_Tag(invoices.get(0).getId(), ObjectType.INVOICE);
+
+        //Verify balance=0 after setting the WRITTEN_OFF tag
+        invoice = invoiceApi.getInvoice(invoices.get(0).getId(), callContext);
+        assertEquals(invoice.getBalance().stripTrailingZeros().compareTo(BigDecimal.ZERO), 0);
+
+        invoice = invoiceApi.getInvoiceByNumber(invoice.getInvoiceNumber(), callContext);
+        assertEquals(invoice.getBalance().stripTrailingZeros().compareTo(BigDecimal.ZERO), 0);
     }
 
     private void add_WRITTEN_OFF_Tag(final UUID id, final ObjectType type) throws TagDefinitionApiException, TagApiException {
