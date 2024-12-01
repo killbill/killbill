1dbd84ebaaf2f71c7a5df033e8022fb3e29dc68a
Bo Oo <bhoo@iastate.edu>
2024-12-01 00:04:27 -0600

    edited the bash file

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Navigate to the project root directory
-cd /home/site/wwwroot
 
 # Run the Maven command
  mvn -Dorg.killbill.server.properties=file:///PROJECT_ROOT/profiles/killbill/src/main/resources/killbill-server.properties -Dlogback.configurationFile=./profiles/killbill/src/main/resources/logback.xml jetty:run
