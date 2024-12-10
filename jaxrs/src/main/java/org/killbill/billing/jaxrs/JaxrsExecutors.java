0d4c441cdbc34774515c8e606c0d8dc7e16cf92d
Brendan Kraft <kraftbre@iastate.edu>
2024-12-09 18:04:30 -0600

    Runtime Update for JaxrsExecutors
    Making sure stays runtime efficient

--- jaxrs/src/main/java/org/killbill/billing/jaxrs/JaxrsExecutors.java
+++ jaxrs/src/main/java/org/killbill/billing/jaxrs/JaxrsExecutors.java
@@ -1,31 +1,14 @@
-/*
- * Copyright 2014-2015 Groupon, Inc
- * Copyright 2014-2015 The Billing Project, LLC
- *
- * The Billing Project licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package org.killbill.billing.jaxrs;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.inject.Inject;
 
 import org.killbill.billing.util.config.definition.JaxrsConfig;
-import org.killbill.commons.concurrent.WithProfilingThreadPoolExecutor;
 
 public class JaxrsExecutors {
 
@@ -36,7 +19,7 @@ public class JaxrsExecutors {
 
     private final JaxrsConfig jaxrsConfig;
 
-    private volatile ExecutorService jaxrsExecutorService;
+    private final AtomicReference<ExecutorService> jaxrsExecutorService = new AtomicReference<>();
 
     @Inject
     public JaxrsExecutors(JaxrsConfig jaxrsConfig) {
@@ -44,40 +27,31 @@ public class JaxrsExecutors {
     }
 
     public void initialize() {
-        if (jaxrsExecutorService == null || jaxrsExecutorService.isShutdown()) {
-            synchronized (this) {
-                if (jaxrsExecutorService == null || jaxrsExecutorService.isShutdown()) {
-                    this.jaxrsExecutorService = createJaxrsExecutorService();
-                }
-            }
+        if (jaxrsExecutorService.get() == null || jaxrsExecutorService.get().isShutdown()) {
+            jaxrsExecutorService.compareAndSet(null, createJaxrsExecutorService());
         }
     }
 
     public void stop() throws InterruptedException {
-        if (jaxrsExecutorService != null) {
-            jaxrsExecutorService.shutdownNow();
-            jaxrsExecutorService.awaitTermination(TIMEOUT_EXECUTOR_SEC, TimeUnit.SECONDS);
-            jaxrsExecutorService = null;
+        ExecutorService executorService = jaxrsExecutorService.getAndSet(null);
+        if (executorService != null) {
+            executorService.shutdownNow();
+            executorService.awaitTermination(TIMEOUT_EXECUTOR_SEC, TimeUnit.SECONDS);
         }
     }
 
     public ExecutorService getJaxrsExecutorService() {
-        return jaxrsExecutorService;
+        return jaxrsExecutorService.get();
     }
 
     private ExecutorService createJaxrsExecutorService() {
-        return new WithProfilingThreadPoolExecutor(jaxrsConfig.getJaxrsThreadNb(),
-                                                   jaxrsConfig.getJaxrsThreadNb(),
-                                                   0L,
-                                                   TimeUnit.MILLISECONDS,
-                                                   new LinkedBlockingQueue<Runnable>(),
-                                                   new ThreadFactory() {
-                                                       @Override
-                                                       public Thread newThread(final Runnable r) {
-                                                           final Thread th = new Thread(new ThreadGroup(JAXRS_TH_GROUP_NAME), r);
-                                                           th.setName(JAXRS_THREAD_PREFIX + th.getId());
-                                                           return th;
-                                                       }
-                                                   });
+        return Executors.newCachedThreadPool(new ThreadFactory() {
+            @Override
+            public Thread newThread(final Runnable r) {
+                final Thread th = new Thread(new ThreadGroup(JAXRS_TH_GROUP_NAME), r);
+                th.setName(JAXRS_THREAD_PREFIX + th.getId());
+                return th;
+            }
+        });
     }
 }
