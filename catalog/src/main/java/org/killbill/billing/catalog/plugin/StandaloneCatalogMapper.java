74addc1c6c1c07f9d7d04b9e0a26cfb07eec0cb6
reshmabidikar <reshmabidikar@outlook.com>
2025-01-22 12:04:57 +0530

    Changes for 1944

--- catalog/src/main/java/org/killbill/billing/catalog/plugin/StandaloneCatalogMapper.java
+++ catalog/src/main/java/org/killbill/billing/catalog/plugin/StandaloneCatalogMapper.java
@@ -236,7 +236,7 @@ public class StandaloneCatalogMapper {
     }
 
     private Collection<Product> toFilteredDefaultProduct(final Collection<Product> input) {
-        if (!input.iterator().hasNext()) {
+        if (input == null || !input.iterator().hasNext()) {
             return Collections.emptyList();
         }
         final Iterable<String> inputProductNames = input.stream()
@@ -351,7 +351,9 @@ public class StandaloneCatalogMapper {
         result.setRecurringBillingMode(input.getRecurringBillingMode());
         result.setEffectiveDateForExistingSubscriptions(input.getEffectiveDateForExistingSubscriptions());
         result.setFinalPhase(toDefaultPlanPhase(input.getFinalPhase()));
-        result.setInitialPhases(toDefaultPlanPhases(List.of(input.getInitialPhases())));
+        if(input.getInitialPhases() != null && input.getInitialPhases().length != 0) {
+            result.setInitialPhases(toDefaultPlanPhases(List.of(input.getInitialPhases())));
+        }
         result.setPlansAllowedInBundle(input.getPlansAllowedInBundle());
         result.setProduct(toDefaultProduct(input.getProduct()));
         result.setPriceListName(input.getPriceList().getName());
