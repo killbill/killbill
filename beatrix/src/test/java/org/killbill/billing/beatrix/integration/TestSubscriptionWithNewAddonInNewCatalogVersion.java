f5a9402becc2201b0b0899271b62e7d216a348ad
reshmabidikar <reshmabidikar@outlook.com>
2025-04-29 07:33:22 +0530

    separate test with workaround

--- beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestSubscriptionWithNewAddonInNewCatalogVersion.java
+++ beatrix/src/test/java/org/killbill/billing/beatrix/integration/TestSubscriptionWithNewAddonInNewCatalogVersion.java
@@ -96,6 +96,42 @@ public class TestSubscriptionWithNewAddonInNewCatalogVersion extends TestIntegra
         clock.addMonths(1);
         assertListenerStatus();
 
+//        //try to add new addon - test fails here
+//        final PlanPhaseSpecifier ao2Spec = new PlanPhaseSpecifier("standard-ao2-monthly", null);
+//        busHandler.pushExpectedEvents(NextEvent.CREATE, NextEvent.BLOCK, NextEvent.INVOICE, NextEvent.INVOICE_PAYMENT,
+//                                      NextEvent.PAYMENT);
+//        entitlementApi.addEntitlement(baseEntitlement.getBundleId(), new DefaultEntitlementSpecifier(ao2Spec, null, null, UUID.randomUUID().toString(), null), clock.getUTCToday(), clock.getUTCToday(), false, Collections.emptyList(), testCallContext);
+//        assertListenerStatus();
+
+    }
+
+    @Test(groups = "slow")
+    public void testCreateSubscriptionWithNewAddOnInNewCatalogVersionChangePlanWorkAround() throws Exception {
+
+        busHandler.pushExpectedEvents(NextEvent.CREATE, NextEvent.BLOCK, NextEvent.INVOICE, NextEvent.INVOICE_PAYMENT,
+                                      NextEvent.PAYMENT);
+
+        //create base
+        final PlanPhaseSpecifier spec = new PlanPhaseSpecifier("standard-monthly", null);
+        final UUID entitlementId = entitlementApi.createBaseEntitlement(account.getId(), new DefaultEntitlementSpecifier(spec, null, null, UUID.randomUUID().toString(), null), "base", clock.getUTCToday(), clock.getUTCToday(), false, true, Collections.emptyList(), testCallContext);
+        Entitlement baseEntitlement = entitlementApi.getEntitlementForId(entitlementId, false, testCallContext);
+        assertEquals(baseEntitlement.getState(), EntitlementState.ACTIVE);
+        assertListenerStatus();
+
+        //add ao1
+        final PlanPhaseSpecifier ao1Spec = new PlanPhaseSpecifier("standard-ao-monthly", null);
+        busHandler.pushExpectedEvents(NextEvent.CREATE, NextEvent.BLOCK, NextEvent.INVOICE, NextEvent.INVOICE_PAYMENT,
+                                      NextEvent.PAYMENT);
+        entitlementApi.addEntitlement(baseEntitlement.getBundleId(), new DefaultEntitlementSpecifier(ao1Spec, null, null, UUID.randomUUID().toString(), null), null, null, false, Collections.emptyList(), testCallContext);
+        assertListenerStatus();
+
+        uploadCatalog("monthly-no-trial-with-addon-v2.xml");
+
+        //move to 2025-02-01 - v2 is active
+        busHandler.pushExpectedEvents(NextEvent.INVOICE, NextEvent.INVOICE_PAYMENT, NextEvent.PAYMENT);
+        clock.addMonths(1);
+        assertListenerStatus();
+
         //change plan to the same plan so it is associated with the new catalog
         busHandler.pushExpectedEvents(NextEvent.CHANGE, NextEvent.NULL_INVOICE);
         baseEntitlement.changePlanWithDate(new DefaultEntitlementSpecifier(spec, null, null, UUID.randomUUID().toString(), null), clock.getUTCToday(), Collections.emptyList(), testCallContext);
