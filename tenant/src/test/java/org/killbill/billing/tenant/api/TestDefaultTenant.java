abb8330f8e49fbcfb2f63a5ee684a4026467a465
xsalefter <xsalefter@gmail.com>
2026-07-19 03:33:13 +0700

    refactor externalizable test to reflection to avoid netty deps

--- tenant/src/test/java/org/killbill/billing/tenant/api/TestDefaultTenant.java
+++ tenant/src/test/java/org/killbill/billing/tenant/api/TestDefaultTenant.java
@@ -18,6 +18,7 @@
 package org.killbill.billing.tenant.api;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 import org.killbill.billing.tenant.TenantTestSuiteNoDB;
@@ -26,16 +27,42 @@ import org.redisson.codec.SerializationCodec;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.netty.buffer.ByteBuf;
-
 public class TestDefaultTenant extends TenantTestSuiteNoDB {
 
     @Test(groups = "fast")
-    public void testExternalizable() throws IOException {
-        final DefaultTenant tenantdata = new DefaultTenant(UUID.randomUUID(), clock.getUTCNow(), clock.getUTCNow(), "er44TT-yy4r", "TTR445ee2", null);
-        final Codec code = new SerializationCodec();
-        final ByteBuf byteBuf = code.getValueEncoder().encode(tenantdata);
-        final DefaultTenant tenantData2 = (DefaultTenant) code.getValueDecoder().decode(byteBuf, null);
-        Assert.assertEquals(tenantData2, tenantdata);
+    public void testExternalizable() throws Throwable {
+        final Codec codec = new SerializationCodec();
+
+        final DefaultTenant tenantData = new DefaultTenant(UUID.randomUUID(), clock.getUTCNow(), clock.getUTCNow(), "er44TT-yy4r", "TTR445ee2", null);
+        final DefaultTenant tenantData2 = externalize(codec, tenantData);
+        Assert.assertEquals(tenantData2, tenantData);
+
+        final DefaultTenant withSecret = new DefaultTenant(UUID.randomUUID(), clock.getUTCNow(), clock.getUTCNow(), "external", "apiKey", "apiSecret");
+        final DefaultTenant withSecret2 = externalize(codec, withSecret);
+        Assert.assertNotEquals(withSecret2, withSecret);
+        Assert.assertNotNull(withSecret.getApiSecret());
+        Assert.assertNull(withSecret2.getApiSecret());
+    }
+
+    private DefaultTenant externalize(final Codec codec, final DefaultTenant tenant) throws Throwable {
+        // Keep the Redisson codec path, but avoid a direct test dependency on Netty's ByteBuf.
+        final Object encoded = invokeRedissonCodecMethod(codec.getValueEncoder(), "encode", tenant);
+        return (DefaultTenant) invokeRedissonCodecMethod(codec.getValueDecoder(), "decode", encoded, null);
+    }
+
+    private Object invokeRedissonCodecMethod(final Object target, final String methodName, final Object... args) throws Throwable {
+        final Method method = findMethod(target.getClass(), methodName, args.length);
+
+        method.setAccessible(true);
+        return method.invoke(target, args);
+    }
+
+    private Method findMethod(final Class<?> clazz, final String methodName, final int argumentCount) throws IOException {
+        for (final Method method : clazz.getDeclaredMethods()) {
+            if (methodName.equals(method.getName()) && method.getParameterCount() == argumentCount) {
+                return method;
+            }
+        }
+        throw new IOException("Unable to find Redisson codec method " + methodName + " on " + clazz.getName());
     }
 }
